99daa7a06579d414168bd7070d26257e0423da64
Preston Mueller <skierjunk@gmail.com>
2020-02-03 00:55:56 -0500

    Switch from Terraform to cloudformation

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
+set -e
+
 export AWS_PROFILE=transitmatters
 export AWS_REGION=us-east-1
 
-pushd server/
-chalice package --pkg-format terraform terraform/
-pushd terraform/
-terraform plan
-
-popd
-popd
-
+pushd server/ > /dev/null
+chalice package --merge-template frontend-cfn.json cfn/
+aws cloudformation package --template-file cfn/sam.json --s3-bucket datadashboard-backend --output-template-file cfn/packaged.yaml
+aws cloudformation deploy --template-file cfn/packaged.yaml --stack-name datadashboard --capabilities CAPABILITY_IAM
+popd > /dev/null
+aws s3 sync build/ s3://dashboard.transitmatters.org
