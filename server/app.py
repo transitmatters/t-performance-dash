2deb76b0fe148e852d05a0925e7c21de676f9a8d
Preston Mueller <skierjunk@gmail.com>
2020-02-05 22:55:22 -0500

    Add CORS configuration

--- server/app.py
+++ server/app.py
@@ -1,9 +1,14 @@
 import json
-from chalice import Chalice
+from chalice import Chalice, CORSConfig
 from datetime import datetime, timedelta, date, time
 from chalicelib import data_funcs
 
 app = Chalice(app_name='data-dashboard')
+
+cors_config = CORSConfig(
+    allow_origin='http://dashboard.transitmatters.org',
+    max_age=3600
+)
 
 def destructure_date(date):
     date_split = date.split('-')
@@ -13,21 +18,21 @@ def destructure_date(date):
         'day': int(date_split[2])
     }
 
-@app.route("/headways/{user_date}")
+@app.route("/headways/{user_date}", cors=cors_config)
 def headways_route(user_date):
     station = app.current_request.query_params.get('station')
     parsed_date = destructure_date(user_date)
     return data_funcs.headways(date(year=parsed_date['year'], month=parsed_date['month'], day=parsed_date['day']), {"stop": station})
 
 
-@app.route("/dwells/{user_date}")
+@app.route("/dwells/{user_date}", cors=cors_config)
 def dwells_route(user_date):
     station = app.current_request.query_params.get('station')
     parsed_date = destructure_date(user_date)
     return data_funcs.dwells(date(year=parsed_date['year'], month=parsed_date['month'], day=parsed_date['day']), {"stop": station})
 
 
-@app.route("/traveltimes/{user_date}")
+@app.route("/traveltimes/{user_date}", cors=cors_config)
 def traveltime_route(user_date):
     station_from = app.current_request.query_params.get('station_from')
     station_to = app.current_request.query_params.get('station_to')
