1baf762d2460c16b1d72dc6a1e55487345c069c2
Preston Mueller <skierjunk@gmail.com>
2020-04-25 15:23:17 -0400

    Use pipenv run before chalice package

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export AWS_PROFILE=transitmatters
 export AWS_REGION=us-east-1
 
 pushd server/ > /dev/null
-chalice package --merge-template frontend-cfn.json cfn/
+pipenv run chalice package --merge-template frontend-cfn.json cfn/
 aws cloudformation package --template-file cfn/sam.json --s3-bucket datadashboard-backend --output-template-file cfn/packaged.yaml
 aws cloudformation deploy --template-file cfn/packaged.yaml --stack-name datadashboard --capabilities CAPABILITY_IAM
 popd > /dev/null
