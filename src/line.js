20c1fa2eda9cf2c7b896dce72636307825d22f2c
Preston Mueller <skierjunk@gmail.com>
2020-05-09 12:46:26 -0400

    Chart size updates and right side legend

--- src/line.js
+++ src/line.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import Legend from './Legend';
 
 const departure_from_normal_string = (metric, benchmark) => {
   const ratio = metric/benchmark;
@@ -47,84 +48,88 @@ class LineClass extends React.Component {
 
     let labels = this.props.data.map(item => item[this.props.xField]);
     return (
-      <div className="chart-container">
-        <Line
-          legend={{ display: false }}
-          data={{
-            labels,
-            datasets: [
-              {
-                label: this.props.seriesName,
-                fill: false,
-                lineTension: 0.1,
-                pointBackgroundColor: point_colors(this.props.data, this.props.yField, this.props.benchmarkField),
-                pointHoverRadius: 3,
-                pointHoverBackgroundColor: point_colors(this.props.data, this.props.yField, this.props.benchmarkField),
-                pointRadius: 3,
-                pointHitRadius: 10,
-                data: this.props.data.map(item => (item[this.props.yField] / 60).toFixed(2))
-              },
-              {
-                label: `${this.props.seriesName}_benchmark`,
-                data: this.props.data.map(item => item[this.props.benchmarkField]/60),
-                pointRadius: 0
-              }
-            ]
-          }}
-          options={{
-            responsive: true,
-            title: {
-              display: true,
-              text: this.props.title,
-              fontSize: 16
-            },
-            tooltips: {
-              mode: "index",
-              callbacks: {
-                title: (tooltipItems, _) => {
-                  return new Date(tooltipItems[0].xLabel).toLocaleTimeString();
-                },
-                label: (tooltipItem, _) => {
-                  console.log("tooltipItem: ", tooltipItem);
-                  if (tooltipItem.datasetIndex === 0) {
-                    return `Actual ${this.props.tooltipUnit}: ${parseFloat(tooltipItem.value).toFixed(2)}`;
-                  }
-                  return `Benchmark MBTA ${this.props.tooltipUnit}: ${parseFloat(tooltipItem.value).toFixed(2)}`;
-                },
-                afterBody: (tooltipItems) => {
-                  if (tooltipItems.length > 1) {
-                    return departure_from_normal_string(tooltipItems[0].value, tooltipItems[1].value);
-                  }
-                }
-              }
-            },
-            scales: {
-              yAxes: [
+      <div className="chart">
+        <div className="chart-container">
+          <Line
+            legend={{ display: false }}
+            data={{
+              labels,
+              datasets: [
                 {
-                  scaleLabel: {
-                    display: true,
-                    fontSize: 14,
-                    labelString: this.props.yFieldLabel
-                  }
-                }
-              ],
-              xAxes: [
+                  label: this.props.seriesName,
+                  fill: false,
+                  lineTension: 0.1,
+                  pointBackgroundColor: point_colors(this.props.data, this.props.yField, this.props.benchmarkField),
+                  pointHoverRadius: 3,
+                  pointHoverBackgroundColor: point_colors(this.props.data, this.props.yField, this.props.benchmarkField),
+                  pointRadius: 3,
+                  pointHitRadius: 10,
+                  data: this.props.data.map(item => (item[this.props.yField] / 60).toFixed(2))
+                },
                 {
-                  type: 'time',
-                  time: {
-                    unit: 'hour',
-                    unitStepSize: 1
-                  },
-                  scaleLabel: {
-                    display: true,
-                    fontSize: 14,
-                    labelString: this.props.xFieldLabel
-                  }
+                  label: `${this.props.seriesName}_benchmark`,
+                  data: this.props.data.map(item => item[this.props.benchmarkField]/60),
+                  pointRadius: 0
                 }
               ]
-            }
-          }}
-        />
+            }}
+            options={{
+              responsive: true,
+              maintainAspectRatio: false,
+              title: {
+                display: true,
+                text: this.props.title,
+                fontSize: 16
+              },
+              tooltips: {
+                mode: "index",
+                callbacks: {
+                  title: (tooltipItems, _) => {
+                    return new Date(tooltipItems[0].xLabel).toLocaleTimeString();
+                  },
+                  label: (tooltipItem, _) => {
+                    console.log("tooltipItem: ", tooltipItem);
+                    if (tooltipItem.datasetIndex === 0) {
+                      return `Actual ${this.props.tooltipUnit}: ${parseFloat(tooltipItem.value).toFixed(2)}`;
+                    }
+                    return `Benchmark MBTA ${this.props.tooltipUnit}: ${parseFloat(tooltipItem.value).toFixed(2)}`;
+                  },
+                  afterBody: (tooltipItems) => {
+                    if (tooltipItems.length > 1) {
+                      return departure_from_normal_string(tooltipItems[0].value, tooltipItems[1].value);
+                    }
+                  }
+                }
+              },
+              scales: {
+                yAxes: [
+                  {
+                    scaleLabel: {
+                      display: true,
+                      fontSize: 14,
+                      labelString: this.props.yFieldLabel
+                    }
+                  }
+                ],
+                xAxes: [
+                  {
+                    type: 'time',
+                    time: {
+                      unit: 'hour',
+                      unitStepSize: 1
+                    },
+                    scaleLabel: {
+                      display: true,
+                      fontSize: 14,
+                      labelString: this.props.xFieldLabel
+                    }
+                  }
+                ]
+              }
+            }}
+          />
+        </div>
+        <Legend />
       </div>
     );
   }
