6eb336b02b2b7e2d67a3d1f4170dc032e80a4fbc
Preston Mueller <skierjunk@gmail.com>
2020-06-14 16:56:09 -0400

    Adjust alerts bar and add tippy

--- src/AlertBar.js
+++ src/AlertBar.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
 import LegendAlerts from './LegendAlerts';
 
-const BAR_STEPS = 200;
 
 function BoxSection(props) {
 
   return (
-    <div title={props.title} style={
-      {
-        "width": "0.5%",
-        "height": "100%",
-        "backgroundColor": props.color,
-        "float": "left",
-        "borderLeft": props.border && "0.1px solid white",
-      }
-    }></div>
+    <Tippy content={props.title}>
+        <div style={
+          {
+            "width": `${props.width}%`,
+            "height": "100%",
+            "backgroundColor": props.color,
+            "float": "left",
+            "position": "absolute",
+            "left": `${props.left}%`,
+            "borderLeft": props.border && "0.1px solid white",
+            "borderRight": props.border && "0.1px solid white",
+          }
+        }>
+      </div>
+    </Tippy>
+
   );
 }
 
@@ -30,34 +38,22 @@ export default function AlertBar(props) {
   const duration = (end - start);
 
   const boxes = [];
-  for(let i = 0; i < BAR_STEPS; i++) {
-    const time_pos = ((i/BAR_STEPS) * duration + start);
+  for (const alert of alerts) {
+    const alert_start = new Date(alert.valid_from).getTime();
+    const alert_end = new Date(alert.valid_to).getTime();
+    const width = (alert_end - alert_start) / duration * 100;
 
-    let found_alert = null;
-    for (const alert of alerts) {
-      const alert_start = new Date(alert.valid_from).getTime();
-      const alert_end = new Date(alert.valid_to).getTime();
-      if (time_pos > alert_start && time_pos < alert_end) {
-        found_alert = alert;
-      }
-    }
+    const left_pos = (alert_start - start) / (duration) * 100;
+    const tooltip = `${new Date(alert.valid_from).toLocaleTimeString('en-US')} - ${new Date(alert.valid_to).toLocaleTimeString('en-US')}\n${alert.text}`;
 
-    let color = "#8c8c8c";
-    let tooltip = "";
-    if (found_alert) {
-      color = "#e53a0b";
-      tooltip = `${new Date(found_alert.valid_from).toLocaleTimeString('en-US')} - ${new Date(found_alert.valid_to).toLocaleTimeString('en-US')}\n${found_alert.text}`;
-    }
-
-    const border = i > 0 && boxes[i - 1][1] !== found_alert;
-    boxes.push([<BoxSection title={tooltip} border={border} color={color} />, found_alert]);
+    boxes.push(<BoxSection title={tooltip} border={true} width={width} left={left_pos} color={"#e53a0b"} />);
   }
 
     return (
       <div className="alerts">
         <div className="alerts-bar-container">
           <div className="alerts-bar">
-            {boxes.map(x => x[0])}
+            {boxes}
           </div>
         </div>
           <LegendAlerts />
