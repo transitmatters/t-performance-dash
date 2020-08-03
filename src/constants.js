66a42ad26119e6740eb28d7927ce270a6e05397c
Preston Mueller <skierjunk@gmail.com>
2020-08-02 23:16:16 -0400

    Update BL preset

--- src/constants.js
+++ src/constants.js
@@ -1100,15 +1100,15 @@ const createConfigPresetValue = (line, fromStationName, toStationName, date) => 
 
 export const configPresets = [
 	{
-		label: "July 13 2020 — Red Line",
+		label: "July 13, 2020 — Red Line",
 		value: createConfigPresetValue("Red", "Central", "Davis", '2020-07-13'),
 	},
 	{
-		label: "July 14 2020 — Orange Line",
+		label: "July 14, 2020 — Orange Line",
 		value: createConfigPresetValue("Orange", "Back Bay", "Green Street", '2020-07-14'),
 	},
 	{
-		label: "May 2 2020 — Blue Line",
-		value: createConfigPresetValue("Blue", "Revere Beach", "Airport", '2020-05-02'),
+		label: "June 23, 2020 — Blue Line",
+		value: createConfigPresetValue("Blue", "Revere Beach", "Airport", '2020-06-23'),
 	},
 ];
