4794770203f98f0a939e2dd0b54913e10526b252
Nathan Weinberg <nathan2@stwmd.net>
2020-09-26 11:43:23 -0400

    Fixed linting issues with server code

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-import json
 from chalice import Chalice, CORSConfig
-from datetime import datetime, timedelta, date, time
+from datetime import date
 from chalicelib import data_funcs
 
 app = Chalice(app_name="data-dashboard")
