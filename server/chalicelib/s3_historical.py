f889e488bcd5b400c4d946ae8445201214a51977
friendchris <47786654+friendchris@users.noreply.github.com>
2020-10-11 11:50:23 -0400

    Adding Travel Times Historical function

--- server/chalicelib/s3_historical.py
+++ server/chalicelib/s3_historical.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta, date, time
 from chalicelib import s3
+import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar
+import numpy as np
 
 DATE_FORMAT_MASSDOT = "%Y-%m-%d %H:%M:%S"
 DATE_FORMAT_OUT = "%Y/%m/%d %H:%M:%S"
@@ -100,3 +103,47 @@ def travel_times(stop_a, stop_b, year, month, day):
         })
 
     return travel_times
+
+def peak_off_peak(df):
+    cal = USFederalHolidayCalendar()
+    holidays = cal.holidays(start=df['dep_dt'].min(), end=df['dep_dt'].max())
+    
+    df['holiday'] = df['dep_dt'].dt.date.astype('datetime64').isin(holidays.date)
+    df['weekday'] = df['dep_dt'].dt.dayofweek
+    
+    conditions  = [(df['holiday']==False) & (df['weekday'] < 5) & ((df['dep_time']>=time(6,30,0)) & (df['dep_time']<time(9,0,0))), (df['holiday']==False) & (df['weekday'] < 5) & ((df['dep_time']>=time(15,30,0)) & (df['dep_time']<time(18,30,0)))]
+    choices     = ['am_peak', 'pm_peak']
+    df['peak']  = np.select(conditions, choices, default='off_peak')
+    return df
+
+def travel_times_over_time(sdate, edate, stop_a, stop_b):
+    all_data = []
+    delta = edate - sdate       # as timedelta
+    
+    # get a range of dates
+    for i in range(delta.days + 1):
+        today = sdate + timedelta(days=i)
+        try:
+            data = travel_times(stop_a, stop_b, year=today.year, month=today.month, day=today.day)
+            all_data = all_data+data
+        except:
+            pass
+        
+    # convert to pandas
+    df = pd.DataFrame.from_records(all_data)
+    df['dep_dt']   = pd.to_datetime(df['dep_dt'])
+    df['dep_time'] = pd.to_datetime(df['dep_dt']).dt.time
+    df = peak_off_peak(df)
+    
+    # get summary stats
+    summary_stats      = df.groupby('service_date')['travel_time_sec'].describe() #summary stats for all
+    summary_stats['peak'] = 'all'
+    summary_stats = summary_stats.reset_index() # reset_index
+    summary_stats_peak = df.groupby(['service_date', 'peak'])['travel_time_sec'].describe().reset_index() #summary_stats for peak / off-peak trains
+
+    # combine summary stats
+    summary_stats_final = summary_stats.append(summary_stats_peak)
+    
+    # conver to dict
+    summary_stats_dict = summary_stats_final.to_dict('records')
+    return summary_stats_dict
