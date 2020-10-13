972fe66ad3b6c480b40f65937255d0cd97a0bc99
friendchris <47786654+friendchris@users.noreply.github.com>
2020-10-12 21:08:48 -0400

    Update s3_historical.py
    changes to s3 pulls

--- server/chalicelib/s3_historical.py
+++ server/chalicelib/s3_historical.py
@@ -104,14 +104,16 @@ def travel_times(stop_a, stop_b, year, month, day):
 
     return travel_times
 
-def peak_off_peak(df):
+def train_peak_status(df):
     cal = USFederalHolidayCalendar()
     holidays = cal.holidays(start=df['dep_dt'].min(), end=df['dep_dt'].max())
     
     df['holiday'] = df['dep_dt'].dt.date.astype('datetime64').isin(holidays.date)
     df['weekday'] = df['dep_dt'].dt.dayofweek
     
-    conditions  = [(df['holiday']==False) & (df['weekday'] < 5) & ((df['dep_time']>=time(6,30,0)) & (df['dep_time']<time(9,0,0))), (df['holiday']==False) & (df['weekday'] < 5) & ((df['dep_time']>=time(15,30,0)) & (df['dep_time']<time(18,30,0)))]
+    conditions  = [(df['holiday']==False) & (df['weekday'] < 5) & ((df['dep_time']>=time(6,30,0)) & (df['dep_time']<time(9,0,0))), 
+                   (df['holiday']==False) & (df['weekday'] < 5) & ((df['dep_time']>=time(15,30,0)) & (df['dep_time']<time(18,30,0)))
+                  ]
     choices     = ['am_peak', 'pm_peak']
     df['peak']  = np.select(conditions, choices, default='off_peak')
     return df
@@ -125,7 +127,7 @@ def travel_times_over_time(sdate, edate, stop_a, stop_b):
         today = sdate + timedelta(days=i)
         try:
             data = travel_times(stop_a, stop_b, year=today.year, month=today.month, day=today.day)
-            all_data = all_data+data
+            all_data.extend(data)
         except:
             pass
         
@@ -133,7 +135,7 @@ def travel_times_over_time(sdate, edate, stop_a, stop_b):
     df = pd.DataFrame.from_records(all_data)
     df['dep_dt']   = pd.to_datetime(df['dep_dt'])
     df['dep_time'] = pd.to_datetime(df['dep_dt']).dt.time
-    df = peak_off_peak(df)
+    df = train_peak_status(df)
     
     # get summary stats
     summary_stats      = df.groupby('service_date')['travel_time_sec'].describe() #summary stats for all
