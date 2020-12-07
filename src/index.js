c96dc9288e4e9f42b154e54c036b92e57eee1230
Preston Mueller <skierjunk@gmail.com>
2020-12-06 21:39:41 -0500

    Upgrade stuff

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import AggregateBeta from './AggregateExperiment';
 import OpenSource from './OpenSource';
 import { BrowserRouter, Route } from 'react-router-dom';
 // import TabContainer from './TabContainer';
 
 ReactDOM.render(<BrowserRouter>
 <Route path={["/opensource"]} exact component={OpenSource} />
-<Route path={["/aggregate_beta"]} exact component={AggregateBeta} />
 <Route path={["/rapidtransit", "/"]} exact component={App} />
 </BrowserRouter>, document.getElementById('root'));
