b64ec1a7960f2856d6ea87f6fa95efc53a6299d2
Preston Mueller <skierjunk@gmail.com>
2021-01-24 13:55:11 -0500

    Also grab the https certificate ARNs from secrets

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,10 @@ export AWS_PAGER=""
 
 # Setup environment stuff
 # By default deploy to production, otherwise "./deploy.sh beta" deploys to beta stage
+
 [[ "$1" = "beta" ]] && ENV_SUFFIX="-beta" || ENV_SUFFIX=""
 [[ "$1" = "beta" ]] && CHALICE_STAGE="beta" || CHALICE_STAGE="production"
+[[ "$1" = "beta" ]] && BACKEND_CERT_ARN="$TM_BACKEND_CERT_ARN_BETA" || BACKEND_CERT_ARN="$TM_BACKEND_CERT_ARN"
 
 BACKEND_BUCKET=datadashboard-backend$ENV_SUFFIX
 FRONTEND_HOSTNAME=dashboard$ENV_SUFFIX.transitmatters.org # Must match in .chalice/config.json!
@@ -24,7 +26,7 @@ npm run build
 pushd server/ > /dev/null
 pipenv run chalice package --stage $CHALICE_STAGE --merge-template frontend-cfn.json cfn/
 aws cloudformation package --template-file cfn/sam.json --s3-bucket $BACKEND_BUCKET --output-template-file cfn/packaged.yaml
-aws cloudformation deploy --template-file cfn/packaged.yaml --stack-name $CF_STACK_NAME --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --parameter-overrides TMFrontendHostname=$FRONTEND_HOSTNAME
+aws cloudformation deploy --template-file cfn/packaged.yaml --stack-name $CF_STACK_NAME --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset --parameter-overrides TMFrontendHostname=$FRONTEND_HOSTNAME TMBackendCertArn=$BACKEND_CERT_ARN
 popd > /dev/null
 aws s3 sync build/ s3://$FRONTEND_HOSTNAME
 
