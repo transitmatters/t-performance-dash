915363d9a7cc50751ad1723427d5a77961fb41fe
Austin Paul <austinjpaul@gmail.com>
2021-03-09 22:09:19 -0500

    document make_parallel

--- server/chalicelib/parallel.py
+++ server/chalicelib/parallel.py
@@ -2,11 +2,14 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import datetime
 
-THREAD_COUNT = 5
 
-
-def make_parallel(single_func):
-    # TODO: consider signature... iterator, singleton func should take iter var as 1st arg?
+def make_parallel(single_func, THREAD_COUNT=5):
+    # This function will wrap another function
+    # (similar to a decorator, but we don't want to overwrite the original)
+    # e.g. parallel_func = make_parallel(singleton_func)
+    # singleton_func's first parameter must be the var to multiplex on
+    # and parallel_func will take an iterable in its stead
+    # TODO: handle kwargs?
     def parallel_func(iterable, *args):
         jobs = []
         for i in iterable:
