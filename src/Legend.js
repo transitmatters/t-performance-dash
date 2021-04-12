62c90e4c7c2c0510598b29b1b4d1d032531195ff
Austin Paul <austinjpaul@gmail.com>
2021-04-12 07:18:02 -0400

    reorder legend

--- src/Legend.js
+++ src/Legend.js
@@ -15,8 +15,8 @@ function Legend() {
 function LegendLongTerm() {
   return (
     <div className="legend">
-      <p><span className="legend-dot" style={{"backgroundColor": "#bfc8d6"}}></span> Interquartile range</p>
       <p><span className="legend-dot" style={{"backgroundColor": "black"}}></span> Median</p>
+      <p><span className="legend-dot" style={{"backgroundColor": "#bfc8d6"}}></span> Interquartile range</p>
     </div>
   );
 }
