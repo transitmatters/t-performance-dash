c882755d493bf0fab6f7413c7f6b46b958d583a3
arosgood <43154850+arosgood@users.noreply.github.com>
2021-05-10 22:47:19 -0400

    Adding open source attribution

--- src/OpenSource.js
+++ src/OpenSource.js
@@ -127,6 +127,65 @@ export default function OpenSource() {
       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br />
       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE<br />
       SOFTWARE.<br />
+        
+      <hr />
+      MIT License<br />
+      <br />
+      Copyright (c) 2019 react-csv<br />
+      <br />
+      Permission is hereby granted, free of charge, to any person obtaining a copy<br />
+      of this software and associated documentation files (the "Software"), to deal<br />
+      in the Software without restriction, including without limitation the rights<br />
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell<br />
+      copies of the Software, and to permit persons to whom the Software is<br />
+      furnished to do so, subject to the following conditions:<br />
+      <br />
+      The above copyright notice and this permission notice shall be included in all<br />
+      copies or substantial portions of the Software.<br />
+      <br />
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br />
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,<br />
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE<br />
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER<br />
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br />
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE<br />
+      SOFTWARE.<br />
+
+      <hr />
+      Font Awesome Free License<br />
+      -------------------------<br />
+      <br />
+      Font Awesome Free is free, open source, and GPL friendly. You can use it for<br />
+      commercial projects, open source projects, or really almost whatever you want.<br />
+      Full Font Awesome Free license: https://fontawesome.com/license/free.<br />
+      <br />
+      # Icons: CC BY 4.0 License (https://creativecommons.org/licenses/by/4.0/)<br />
+      In the Font Awesome Free download, the CC BY 4.0 license applies to all icons<br />
+      packaged as SVG and JS file types.<br />
+      <br />
+      # Fonts: SIL OFL 1.1 License (https://scripts.sil.org/OFL)<br />
+      In the Font Awesome Free download, the SIL OFL license applies to all icons<br />
+      packaged as web and desktop font files.<br />
+      <br />
+      # Code: MIT License (https://opensource.org/licenses/MIT)<br />
+      In the Font Awesome Free download, the MIT license applies to all non-font and<br />
+      non-icon files.<br />
+      <br />
+      # Attribution<br />
+      Attribution is required by MIT, SIL OFL, and CC BY licenses. Downloaded Font<br />
+      Awesome Free files already contain embedded comments with sufficient<br />
+      attribution, so you shouldn't need to do anything additional when using these<br />
+      files normally.<br />
+      <br />
+      We've kept attribution comments terse, so we ask that you do not actively work<br />
+      to remove them from files, especially code. They're a great way for folks to<br />
+      learn about Font Awesome.<br />
+      <br />
+      # Brand Icons<br />
+      All brand icons are trademarks of their respective owners. The use of these<br />
+      trademarks does not indicate endorsement of the trademark holder by Font<br />
+      Awesome, nor vice versa. **Please do not use brand logos for any purpose except<br />
+      to represent the company, product, or service to which they refer.**<br />
 
 
     </div>
