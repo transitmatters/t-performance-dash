5039fff2b72922c5fc7ee66f9d548fe8782518e0
Austin Paul <austinjpaul@gmail.com>
2021-12-05 21:18:29 -0500

    single -> double quotes

--- server/bus/manifest.py
+++ server/bus/manifest.py
@@ -20,7 +20,7 @@ def load_checkpoints(checkpoint_file):
 
 def emit_stop_obj(entry, branches=False):
     tpt_id = entry.name
-    if entry['counts'].sum() < 100:
+    if entry["counts"].sum() < 100:
         # this timepoint is very infrequently used
         return None
 
@@ -37,25 +37,25 @@ def emit_stop_obj(entry, branches=False):
 
 
 def create_manifest(df, routes, checkpoint_file):
-    output_route_name = '/'.join(routes)
+    output_route_name = "/".join(routes)
 
-    df['time_point_id'] = df['time_point_id'].str.lower()
-    df['time_point_order'] = df['time_point_order'].fillna(0)
+    df["time_point_id"] = df["time_point_id"].str.lower()
+    df["time_point_order"] = df["time_point_order"].fillna(0)
 
-    tpts = df[['route_id', 'stop_id', 'time_point_id', 'direction_id']].value_counts(dropna=False).rename("counts").reset_index()
+    tpts = df[["route_id", "stop_id", "time_point_id", "direction_id"]].value_counts(dropna=False).rename("counts").reset_index()
 
     # use checkpoint file to map time_point_id to stop_name
     station_names = load_checkpoints(checkpoint_file)
-    tpts['stop_name'] = tpts['time_point_id'].map(station_names)
+    tpts["stop_name"] = tpts["time_point_id"].map(station_names)
 
-    # Create full stop id e.g. '66-0-64000'
-    tpts['full_stop_id'] = tpts[['route_id', 'direction_id', 'stop_id']].astype(str).agg('-'.join, axis=1)
+    # Create full stop id e.g. "66-0-64000"
+    tpts["full_stop_id"] = tpts[["route_id", "direction_id", "stop_id"]].astype(str).agg("-".join, axis=1)
 
     # Must be arranged by inbound direction. Use most common (mode) as guess (w/ max in case 2 modes)
-    orders = df.loc[df.direction_id == 1].groupby("time_point_id", dropna=False)['time_point_order'].agg(lambda x: x.mode().max())
-    tpts['order_guess'] = tpts['time_point_id'].map(orders).fillna(0).astype(int)
+    orders = df.loc[df.direction_id == 1].groupby("time_point_id", dropna=False)["time_point_order"].agg(lambda x: x.mode().max())
+    tpts["order_guess"] = tpts["time_point_id"].map(orders).fillna(0).astype(int)
 
-    stop_objs = tpts.groupby('time_point_id', dropna=False).apply(
+    stop_objs = tpts.groupby("time_point_id", dropna=False).apply(
         lambda x: emit_stop_obj(x, len(routes) > 1)
     ).dropna()
 
@@ -66,7 +66,7 @@ def create_manifest(df, routes, checkpoint_file):
                 "0": "outbound",
                 "1": "inbound"
             },
-            "stations": sorted(stop_objs.values, key=lambda x: x['order'])
+            "stations": sorted(stop_objs.values, key=lambda x: x["order"])
         }
     }
 
@@ -76,10 +76,10 @@ def create_manifest(df, routes, checkpoint_file):
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('input', metavar='INPUT_CSV')
-    parser.add_argument('output', metavar='OUTPUT_JSON')
-    parser.add_argument('--routes', '-r', nargs="+", type=str)
-    parser.add_argument('--checkpoints', metavar="gtfs_checkpoints.txt")
+    parser.add_argument("input", metavar="INPUT_CSV")
+    parser.add_argument("output", metavar="OUTPUT_JSON")
+    parser.add_argument("--routes", "-r", nargs="+", type=str)
+    parser.add_argument("--checkpoints", metavar="gtfs_checkpoints.txt")
 
     args = parser.parse_args()
     input_csv = args.input
