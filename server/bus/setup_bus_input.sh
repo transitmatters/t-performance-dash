8960df19c6bf52ff3262dd17f9cbe3504fd769b9
Christopher Schmidt <crschmidt@crschmidt.net>
2021-12-20 09:26:21 -0500

    Minor cleanups to data import tool.

--- server/bus/setup_bus_input.sh
+++ server/bus/setup_bus_input.sh
@@ -14,16 +14,20 @@ cd data/input
 for i in 2021 2020 2019 2018; do
   unzip -d $i $i.zip
 done
-mv 2021/MBTA*/*.csv 2021/
 
-mv "2020/Bus Arrival Departure Times Jan-Mar 2020.csv" "2020/2020-01-03.csv"
-mv "2020/Bus Arrival Departure Times Apr-June 2020.csv" "2020/2020-04-06.csv"
-mv "2020/Bus Arrival Departure Times Jul-Sep 2020.csv" "2020/2020-07-09.csv"
-mv "2020/Bus Arrival Departure Times Oct-Dec 2020.csv" "2020/2020-10-12.csv"
-mv "2019/MBTA Bus Arrival Departure Jan-Mar 2019.csv" "2019/2019-01-03.csv"
-mv "2019/MBTA Bus Arrival Departure Apr-June 2019.csv" "2019/2019-04-06.csv"
-mv "2019/MBTA Bus Arrival Departure Jul-Sept 2019.csv" "2019/2019-07-09.csv"
-mv "2019/MBTA Bus Arrival Departure Oct-Dec 2019.csv" "2019/2019-10-12.csv"
-mv "2018/MBTA Bus Arrival Departure Aug-Sept 2018.csv" "2018/2018-08-09.csv"
-mv "2018/MBTA Bus Arrival Departure Oct-Dec 2018.csv" "2018/2018-10-12.csv"
-sed -i -e 's/<U+FEFF>//' 2020/2020-07-09.csv
+mv 2021/MBTA*/*.csv 2021/
+rmdir 2021/MBTA_Bus_Arrival_Depature_Times_2021/
+
+# Rename files so they have consistent names and no spaces.
+mv "2020/Bus Arrival Departure Times Jan-Mar 2020.csv" "2020/2020-Q1.csv"
+mv "2020/Bus Arrival Departure Times Apr-June 2020.csv" "2020/2020-Q2.csv"
+mv "2020/Bus Arrival Departure Times Jul-Sep 2020.csv" "2020/2020-Q3.csv"
+mv "2020/Bus Arrival Departure Times Oct-Dec 2020.csv" "2020/2020-Q4.csv"
+mv "2019/MBTA Bus Arrival Departure Jan-Mar 2019.csv" "2019/2019-Q1.csv"
+mv "2019/MBTA Bus Arrival Departure Apr-June 2019.csv" "2019/2019-Q2.csv"
+mv "2019/MBTA Bus Arrival Departure Jul-Sept 2019.csv" "2019/2019-Q3.csv"
+mv "2019/MBTA Bus Arrival Departure Oct-Dec 2019.csv" "2019/2019-Q4.csv"
+mv "2018/MBTA Bus Arrival Departure Aug-Sept 2018.csv" "2018/2018-Q3.csv"
+mv "2018/MBTA Bus Arrival Departure Oct-Dec 2018.csv" "2018/2018-Q4.csv"
+sed -i -e 's/<U+FEFF>//' 2020/2020-Q3.csv
+
