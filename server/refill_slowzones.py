e089c7105ca4fe2ffbe7b7b64ffbbf8e9c2c61fa
Austin Paul <austinjpaul@gmail.com>
2022-01-20 16:05:37 -0500

    add script to backfill slowzones when nightly fill doesn't work

---
+++ server/refill_slowzones.py
@@ -0,0 +1,30 @@
+import datetime
+
+from chalicelib.parallel import date_range
+from chalicelib.s3_alerts import store_alerts, get_alerts
+
+
+START = datetime.date(2022,1,3)
+END = datetime.date.today() - datetime.timedelta(days=2)
+
+def do_alerts_exist(d):
+    try:
+        get_alerts(d, ["Red"])
+    except Exception as err:
+        if err.response["Error"]["Code"] == "NoSuchKey":
+            return False
+    return True
+
+
+for d in date_range(START, END):
+    if do_alerts_exist(d):
+        continue
+    print("storing", d)
+    for i in range(3):
+        print("  attempt", i+1, "of 3")
+        try:
+            store_alerts(d)
+            print("success")
+            break
+        except:
+            continue
