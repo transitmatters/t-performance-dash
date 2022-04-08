cea74804871ceef670fa36a9031e235ab65c45bb
Austin Paul <austinjpaul@gmail.com>
2022-04-08 10:59:44 -0400

    update bus2train to output monthly bus files

--- server/bus/gen_bus_data.sh
+++ server/bus/gen_bus_data.sh
@@ -5,7 +5,7 @@ if [ -z "$routes" ]; then
 	routes="1"
 fi
 
-for y in 2018 2019 2020 2021; do
+for y in 2018 2019 2020 2021 2022; do
     for f in $(find data/input/$y/ -name '*.csv'); do
 	echo "Generating stop data from $f"
         poetry run python bus2train.py $f data/output -r $routes
