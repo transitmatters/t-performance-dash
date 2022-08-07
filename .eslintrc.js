667e05f5244b3763448dae039ce5e97616a94e1f
Devin Matte <devinmatte@gmail.com>
2022-08-07 16:58:50 -0400

    Revert "Revert "Updating Eslint config with plugins (#202)""
    This reverts commit 60e2cd2d2b7e55e7cce986753ca44085289e654e.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
-    "extends": "react-app"
-}
+  env: {
+    browser: true,
+    es6: true,
+    amd: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": "off", // TODO: Change to Warn
+    "react/prop-types": "off", // TODO: Turn on or move to TS
+    "react/no-unescaped-entities": "off",
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
