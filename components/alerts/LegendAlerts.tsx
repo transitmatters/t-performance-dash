ab4ef804ad79f55b9c2aaa972930b9d486917247
Devin Matte <devinmatte@gmail.com>
2022-10-21 17:01:46 -0400

    [v4 DEV] Showing alerts and using node 18 (#241)
    * Adding MBTA alerts to dashboard view
    
    This is currently without tooltips because I can't get them to display
    
    * Updating node to 18
    
    * Cleaner css

---
+++ components/alerts/LegendAlerts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const LegendAlerts: React.FC = () => {
+  return (
+    <div className="legend">
+      <p>
+        <span className="legend-line alert-incident-line"></span>
+        MBTA advisory
+      </p>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import { SingleDayLineChart } from '../components/dashboard/charts/SingleDayLine
 import headwaysData from './data/headways.json';
 import travelTimesData from './data/travel_times.json';
 import dwellsData from './data/dwells.json';
+import alerts from './data/alerts.json';
 
 import React from 'react';
+import { AlertBar } from '../components/alerts/AlertBar';
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
@@ -63,6 +65,7 @@ export default function Home() {
           </div>
         </div>
       </main>
+      <AlertBar alerts={alerts} today={'2022-10-11'} isLoading={false} />
       <div className="px-4">
         <div className={'charts main-column'}>
           <SingleDayLineChart
