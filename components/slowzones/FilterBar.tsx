6b45a63dff5e2597c0494bc53059010ceb52ebbe
Seth Kaplan <sethkaplan73@gmail.com>
2022-11-12 11:00:05 -0500

    add gnatt chart WIP

---
+++ components/slowzones/FilterBar.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FilterBar = () => {
+  return <div>FilterBar</div>;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}', 
+    './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      display: ['Helvetica Neue', 'sans-serif'],
+    },
     extend: {
       colors: {
         mbta: {
           red: '#da291c',
+          lightRed: '#ea6359',
           orange: '#ed8b00',
           blue: '#003da5',
           green: '#00834d',
+        },
+        tm: {
+          red: '#a31e1e',
+          light_red: '#cb2525',
+          grey: '#2e2d2c',
         },
       },
     },
