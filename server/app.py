ca86afb6d9e263441b210c8b9b860d47508c997c
Nathan Weinberg <nathan2@stwmd.net>
2022-11-13 12:37:53 -0500

    Added Preston's PerfAPI check

--- server/app.py
+++ server/app.py
@@ -2,11 +2,12 @@ import json
 import os
 import subprocess
 from chalice import Chalice, CORSConfig, ConflictError, Response
-from datetime import date
+from datetime import date, timedelta
 from chalicelib import (
     aggregation,
     data_funcs,
-    secrets,
+    MbtaPerformanceAPI,
+    secrets
 )
 
 app = Chalice(app_name="data-dashboard")
@@ -37,7 +38,8 @@ def healthcheck():
     # These functions must return True or False :-)
     checks = {
         "API Key Present": (lambda: len(secrets.MBTA_V2_API_KEY) > 0),
-        "S3 Headway Fetching": (lambda: "2020-11-07 10:33:40" in json.dumps(data_funcs.headways(date(year=2020, month=11, day=7), ["70061"])))
+        "S3 Headway Fetching": (lambda: "2020-11-07 10:33:40" in json.dumps(data_funcs.headways(date(year=2020, month=11, day=7), ["70061"]))),
+        "Performance API Check": (lambda: MbtaPerformanceAPI.get_api_data("headways", {"stop": [70067]}, date.today() - timedelta(days=1), date.today()))
     }
 
     failed_checks = {}
