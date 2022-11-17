9d3d876335da47ba87cf5a95ed260bd2ad1e022b
Patrick Cleary <patcleary11@gmail.com>
2022-11-17 18:50:36 -0500

    Only rewrite when local

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+// If running locally rewrite requests to server port (proxy).
+let rewrites = []
+if(process.env.NODE_ENV === 'development') {
+  rewrites.push({
+      source: '/:path*',
+      destination: 'http://localhost:5000/:path*'
+    });
+}
+
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'http://localhost:5000/:path*'
-      },
-    ]
+ async rewrites() {
+    return rewrites
   },
   reactStrictMode: true,
   swcMinify: true,
