0a2108d49c3066548d5ee197e0ef78e206b8b1f4
Devin Matte <devinmatte@gmail.com>
2022-12-17 15:38:16 -0500

    Fixing legend alignment

--- components/dashboard/charts/AggregateLineChart.tsx
+++ components/dashboard/charts/AggregateLineChart.tsx
@@ -67,115 +67,113 @@ export const AggregateLineChart: React.FC<AggregateLineProps> = ({
   return (
     <div className="chart">
       <div className="chart-container">
-        <div>
-          <Line
-            id={chartId}
-            height={250}
-            data={{
-              labels,
-              datasets: [
-                {
-                  label: seriesName,
-                  fill: false,
-                  tension: 0.1,
-                  pointBackgroundColor: CHART_COLORS.GREY,
-                  pointHoverRadius: 3,
-                  pointHoverBackgroundColor: CHART_COLORS.GREY,
-                  pointRadius: 3,
-                  pointHitRadius: 10,
-                  data: data.map((item: AggregateDataPoint) => (item['50%'] / 60).toFixed(2)),
-                },
-                {
-                  label: '25th percentile',
-                  fill: 1,
-                  backgroundColor: fillColor,
-                  tension: 0.4,
-                  pointRadius: 0,
-                  data: data.map((item: AggregateDataPoint) => (item['25%'] / 60).toFixed(2)),
-                },
-                {
-                  label: '75th percentile',
-                  fill: 1,
-                  backgroundColor: fillColor,
-                  tension: 0.4,
-                  pointRadius: 0,
-                  data: data.map((item: AggregateDataPoint) => (item['75%'] / 60).toFixed(2)),
-                },
-              ],
-            }}
-            options={{
-              scales: {
-                y: {
-                  title: {
-                    display: true,
-                    text: 'Minutes',
-                  },
-                  ticks: {
-                    precision: 1,
-                  },
-                  suggestedMin: suggestedYMin,
-                  suggestedMax: suggestedYMax,
-                },
-                x: {
-                  time: {
-                    unit: timeUnit,
-                    stepSize: 1,
-                    tooltipFormat: timeFormat,
-                  },
-                  type: 'time',
-                  adapters: {
-                    date: {
-                      locale: enUS,
-                    },
-                  },
-                  // force graph to show startDate to endDate, even if missing data
-                  min: hourly ? null : startDate,
-                  max: hourly ? null : endDate,
-                  title: {
-                    display: true,
-                    text: xAxisLabel(startDate ?? '', endDate ?? '', hourly),
-                  },
-                },
-              },
-              responsive: true,
-              maintainAspectRatio: false,
-              layout: {
-                padding: {
-                  top: 25,
-                },
-              },
-              // Make the tooltip display all 3 datapoints for each x axis entry.
-              interaction: {
-                mode: 'index',
-                intersect: false,
-              },
-              plugins: {
-                legend: {
-                  display: false,
-                },
-              },
-            }}
-            plugins={[
+        <Line
+          id={chartId}
+          height={250}
+          data={{
+            labels,
+            datasets: [
               {
-                id: 'customTitleAggregate',
-                afterDraw: (chart: ChartJS) => {
-                  // TODO: This is not placing the title correctly for aggregate charts.
-                  drawTitle(
-                    title,
-                    { to: 'Park Street', from: 'Porter', direction: 'southbound', line: 'Red' },
-                    bothStops,
-                    chart
-                  );
+                label: seriesName,
+                fill: false,
+                tension: 0.1,
+                pointBackgroundColor: CHART_COLORS.GREY,
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: CHART_COLORS.GREY,
+                pointRadius: 3,
+                pointHitRadius: 10,
+                data: data.map((item: AggregateDataPoint) => (item['50%'] / 60).toFixed(2)),
+              },
+              {
+                label: '25th percentile',
+                fill: 1,
+                backgroundColor: fillColor,
+                tension: 0.4,
+                pointRadius: 0,
+                data: data.map((item: AggregateDataPoint) => (item['25%'] / 60).toFixed(2)),
+              },
+              {
+                label: '75th percentile',
+                fill: 1,
+                backgroundColor: fillColor,
+                tension: 0.4,
+                pointRadius: 0,
+                data: data.map((item: AggregateDataPoint) => (item['75%'] / 60).toFixed(2)),
+              },
+            ],
+          }}
+          options={{
+            scales: {
+              y: {
+                title: {
+                  display: true,
+                  text: 'Minutes',
+                },
+                ticks: {
+                  precision: 1,
+                },
+                suggestedMin: suggestedYMin,
+                suggestedMax: suggestedYMax,
+              },
+              x: {
+                time: {
+                  unit: timeUnit,
+                  stepSize: 1,
+                  tooltipFormat: timeFormat,
+                },
+                type: 'time',
+                adapters: {
+                  date: {
+                    locale: enUS,
+                  },
+                },
+                // force graph to show startDate to endDate, even if missing data
+                min: hourly ? undefined : startDate,
+                max: hourly ? undefined : endDate,
+                title: {
+                  display: true,
+                  text: xAxisLabel(startDate ?? '', endDate ?? '', hourly),
                 },
               },
-            ]}
-          />
-          {/* TODO: add back download button */}
-        </div>
-        <div className="chart-extras">
-          <LegendLongTerm />
-          {props.children}
-        </div>
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            layout: {
+              padding: {
+                top: 25,
+              },
+            },
+            // Make the tooltip display all 3 datapoints for each x axis entry.
+            interaction: {
+              mode: 'index',
+              intersect: false,
+            },
+            plugins: {
+              legend: {
+                display: false,
+              },
+            },
+          }}
+          plugins={[
+            {
+              id: 'customTitleAggregate',
+              afterDraw: (chart: ChartJS) => {
+                // TODO: This is not placing the title correctly for aggregate charts.
+                drawTitle(
+                  title,
+                  { to: 'Park Street', from: 'Porter', direction: 'southbound', line: 'Red' },
+                  bothStops,
+                  chart
+                );
+              },
+            },
+          ]}
+        />
+        {/* TODO: add back download button */}
+      </div>
+      <div className="chart-extras">
+        <LegendLongTerm />
+        {props.children}
       </div>
     </div>
   );
