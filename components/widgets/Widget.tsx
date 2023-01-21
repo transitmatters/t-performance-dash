0c3b3ac6bc1b2fc9b28b26d8b38c26ace7360a5b
Seth Kaplan <sethkaplan73@gmail.com>
2023-01-21 16:10:47 -0500

    Add first basic slow zone widget

--- components/widgets/Widget.tsx
+++ components/widgets/Widget.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { classNames } from '../utils/tailwind';
 
 export const WidgetPage = ({ children }) => {
-  return <div className={classNames('flex w-full flex-1 flex-col gap-y-2 p-4')}>{children}</div>;
+  return (
+    <div className={classNames('flex w-full flex-1 flex-col gap-y-2 p-4 pb-14')}>{children}</div>
+  );
 };
