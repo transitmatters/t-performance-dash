6c31f7139491a587f81b0c7f09ae9fc103bca5e5
Patrick Cleary <patcleary11@gmail.com>
2023-02-03 14:35:20 -0500

    Add all tailwind default breakpoints to custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
   theme: {
     screens: {
       sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     fontSize: {
       // TODO: try using only defaults.
