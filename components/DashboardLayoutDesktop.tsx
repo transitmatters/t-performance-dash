074e2334df98ab0901638237e290e8feeab91eb3
Seth Kaplan <sethkaplan73@gmail.com>
2023-02-04 09:50:41 -0500

    Remove bottom nav on desktop (#332)

--- components/DashboardLayoutDesktop.tsx
+++ components/DashboardLayoutDesktop.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { SecondaryNavBar } from './general/SecondaryNavBar';
 import { SideNavBar } from './SideNavBar';
+import { useBreakpoint } from './utils/ScreenSize';
 import { DataPageHeader } from './widgets/DataPageHeader';
 import { WidgetPage } from './widgets/Widget';
 
 export const DashboardLayoutDesktop = ({ children }) => {
+  const isMobile = !useBreakpoint('sm');
+
   return (
-    <div className="flex h-full flex-row">
+    <div>
       <SideNavBar />
-      <div className="flex h-full w-full flex-col items-center pl-40">
-        <DataPageHeader dateString="Today (TBD)" />
-        <WidgetPage>{children}</WidgetPage>
-        <SecondaryNavBar />
+      <div className="flex flex-1 flex-col md:pl-48">
+        <main className="flex-1">
+          <div className="py-6">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+              <WidgetPage>
+                <DataPageHeader />
+                {children}
+              </WidgetPage>
+              {isMobile && <SecondaryNavBar />}
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );
