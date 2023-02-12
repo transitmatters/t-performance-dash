2927b1e0b1d586844486f0dad5438e4e58c666e8
Devin Matte <devinmatte@gmail.com>
2023-02-12 16:31:58 -0500

    Adding ridership data to git

--- common/api/ridership.ts
+++ common/api/ridership.ts
@@ -5,7 +5,7 @@ export const fetchAllRidership = (): Promise<{
   summaryData: SummaryData;
   lineData: Record<string, LineData>;
 }> => {
-  const all_ridership_url = new URL(`/static/slowzones/ridership.json`, window.location.origin);
+  const all_ridership_url = new URL(`/ridership.json`, window.location.origin);
   return fetch(all_ridership_url.toString()).then((resp) => resp.json());
 };
 
