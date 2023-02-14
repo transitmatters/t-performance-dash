add84dc43f6b8c7c7bd2c25aebf2428a277f407c
Patrick Cleary <patcleary11@gmail.com>
2023-02-14 11:30:50 -0500

    Update ridership to new widget setup

--- modules/ridership/RidershipWidget.tsx
+++ modules/ridership/RidershipWidget.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import React, { useMemo, useState } from 'react';
 import { useRidershipData } from '../../common/api/ridership';
 import { BasicWidgetDataLayout } from '../../common/components/widgets/internal/BasicWidgetDataLayout';
-import { colorsForLine } from '../../common/constants/colors';
+import { LINE_COLORS } from '../../common/constants/colors';
 import { PercentageWidgetValue, TripsWidgetValue } from '../../common/types/basicWidgets';
 import type { ServiceDay } from '../../common/types/ridership';
 import { getHighestTphValue, normalizeToPercent } from '../../common/utils/ridership';
@@ -14,10 +14,10 @@ import { TphChart } from './charts/TphChart';
 export const RidershipWidget: React.FC = () => {
   const allRidership = useRidershipData();
 
-  const { linePath, lineShort } = useDelimitatedRoute();
+  const { line, linePath, lineShort } = useDelimitatedRoute();
   const lineData = allRidership.data?.lineData[`line-${lineShort}`];
 
-  const color = colorsForLine[lineShort.toLowerCase()] ?? 'black';
+  const color = LINE_COLORS[line ?? 'default'];
   const [serviceDay, setServiceDay] = useState<ServiceDay>('weekday');
   const highestTph = useMemo(() => (lineData ? getHighestTphValue(lineData) : 0), [lineData]);
   const startDate = useMemo(
@@ -28,47 +28,45 @@ export const RidershipWidget: React.FC = () => {
   const serviceHistory = lineData?.serviceHistory ?? [];
 
   return (
-    <>
+    <div className={classNames('h-full rounded-lg bg-white p-2 shadow-dataBox')}>
       <HomescreenWidgetTitle title="Ridership & Service Levels" href={`/${linePath}/ridership`} />
-      <div className={classNames('h-full rounded-lg bg-white p-2 shadow-dataBox')}>
-        <div className={classNames('h-50 pr-4')}>
-          <ServiceRidershipChart lineData={lineData} startDate={startDate} color={color} />
-        </div>
-        <div className={classNames('h-50 pr-4')}>
-          <TphChart
-            lineData={lineData}
-            serviceDay={serviceDay}
-            color={color}
-            highestTph={highestTph}
-          />
-        </div>
-        <div className={classNames('flex w-full flex-row')}>
-          <BasicWidgetDataLayout
-            title="Pre-COVID Ridership"
-            widgetValue={
-              new PercentageWidgetValue(
-                ridershipPercentage[ridershipPercentage.length - 1],
-                ridershipPercentage[ridershipPercentage.length - 1] -
-                  ridershipPercentage[ridershipPercentage.length - 31]
-              )
-            }
-            analysis={`from last month.`}
-            sentimentDirection={'positiveOnIncrease'}
-          />
-          <BasicWidgetDataLayout
-            title="Service Levels"
-            widgetValue={
-              new TripsWidgetValue(
-                serviceHistory?.[serviceHistory.length - 1],
-                serviceHistory?.[serviceHistory.length - 1] -
-                  serviceHistory?.[serviceHistory.length - 365]
-              )
-            }
-            analysis={`since last year.`}
-            sentimentDirection={'positiveOnIncrease'}
-          />
-        </div>
+      <div className={classNames('h-50 pr-4')}>
+        <ServiceRidershipChart lineData={lineData} startDate={startDate} color={color} />
       </div>
-    </>
+      <div className={classNames('h-50 pr-4')}>
+        <TphChart
+          lineData={lineData}
+          serviceDay={serviceDay}
+          color={color}
+          highestTph={highestTph}
+        />
+      </div>
+      <div className={classNames('flex w-full flex-row')}>
+        <BasicWidgetDataLayout
+          title="Pre-COVID Ridership"
+          widgetValue={
+            new PercentageWidgetValue(
+              ridershipPercentage[ridershipPercentage.length - 1],
+              ridershipPercentage[ridershipPercentage.length - 1] -
+                ridershipPercentage[ridershipPercentage.length - 31]
+            )
+          }
+          analysis={`from last month.`}
+          sentimentDirection={'positiveOnIncrease'}
+        />
+        <BasicWidgetDataLayout
+          title="Service Levels"
+          widgetValue={
+            new TripsWidgetValue(
+              serviceHistory?.[serviceHistory.length - 1],
+              serviceHistory?.[serviceHistory.length - 1] -
+                serviceHistory?.[serviceHistory.length - 365]
+            )
+          }
+          analysis={`since last year.`}
+          sentimentDirection={'positiveOnIncrease'}
+        />
+      </div>
+    </div>
   );
 };
