8baf36112e8701dc8c29a30890746258e98d26a8
Patrick Cleary <patcleary11@gmail.com>
2023-02-17 18:34:11 -0500

    remove v4 build

--- next.config.js
+++ next.config.js
@@ -9,12 +9,6 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
-// This is to prevent typescript errors from failing the build while we set up v4.
-const tsSettings = { ignoreBuildErrors: false };
-if (process.env.V4_TEMP) {
-  tsSettings.ignoreBuildErrors = true;
-}
-
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
@@ -30,7 +24,6 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
-  typescript: tsSettings,
   // No nextJS image optimization for a static site.
   images: {
     unoptimized: true,
