e234118c50de7d99deef5b387fd2ada100237499
Patrick Cleary <patcleary11@gmail.com>
2023-03-06 14:46:58 -0500

    Default date selector to today

--- common/components/inputs/DateSelector.tsx
+++ common/components/inputs/DateSelector.tsx
@@ -15,24 +15,26 @@ export const DateSelector: React.FC<DateSelectorProps> = ({ range }) => {
   const updateQueryParams = useUpdateQuery({ range });
   const maxDate = getCurrentDate();
 
-  const dates = useMemo(
-    () => ({
-      startDate: startDate ?? null,
-      endDate: endDate ?? startDate ?? null,
-    }),
-    [startDate, endDate]
-  );
+  const dates = useMemo(() => {
+    return startDate
+      ? {
+          startDate: startDate,
+          endDate: endDate ?? startDate,
+        }
+      : null;
+  }, [startDate, endDate]);
 
   return (
     <Datepicker
       primaryColor={linePath !== 'bus' ? linePath : 'yellow'}
       value={dates}
+      placeholder="Date"
       onChange={(evt) => updateQueryParams(evt)}
       maxDate={maxDate}
       asSingle={!range}
       useRange={range}
       showShortcuts={true}
-      displayFormat={'YYYY-MM-DD'}
+      displayFormat={'M/D/YY'}
       containerClassName={'w-auto'}
       inputClassName={'h-8'}
       i18n={'en-us'}
