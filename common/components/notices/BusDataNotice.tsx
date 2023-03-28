b0c5d6f4224eee0c273b843f24845a21a0cd2424
Devin Matte <devinmatte@gmail.com>
2023-03-27 20:56:00 -0400

    Adding notice about bus data

---
+++ common/components/notices/BusDataNotice.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useDelimitatedRoute } from '../../utils/router';
+
+export const BusDataNotice: React.FC = () => {
+  const { line, linePath } = useDelimitatedRoute();
+
+  if (line === 'BUS' || linePath === 'bus') {
+    return (
+      <div className={'m-3 text-sm italic'}>
+        <p>
+          Due to data collection issues, bus data is not guaranteed to be complete for any stop or
+          date.
+        </p>
+        <p>This may lead to inaccuracies, particularly in headway calculations.</p>
+      </div>
+    );
+  }
+  return null;
+};
