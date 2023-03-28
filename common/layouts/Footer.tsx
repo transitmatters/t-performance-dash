fec6fb525095918e81cdbeb1dc9d71ee933708c6
Devin Matte <devinmatte@gmail.com>
2023-03-28 10:47:41 -0400

    Adding OSS licenses page (#407)
    * Adding OSS licenses page
    
    * Adding footer
    
    * Float footer to bottom
    
    ---------
    
    Co-authored-by: Patrick Cleary <patcleary11@gmail.com>

---
+++ common/layouts/Footer.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import React from 'react';
+
+import { version } from '../../package.json';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="pb-safe mb-24 mt-5 text-center text-sm md:mb-0 md:pb-2 md:pl-64">
+      Version:{' '}
+      <Link
+        href={`https://github.com/transitmatters/t-performance-dash/releases/tag/${version.substring(
+          0,
+          3
+        )}`}
+      >
+        {version}
+      </Link>{' '}
+      |{' '}
+      <Link
+        className={'text-blue-600 hover:text-black dark:text-blue-500'}
+        href="https://transitmatters.org/transitmatters-labs"
+      >
+        TransitMatters Labs
+      </Link>{' '}
+      |{' '}
+      <Link
+        className={'text-blue-600 hover:text-black dark:text-blue-500'}
+        href="https://transitmatters.org/blog/slowzones"
+      >
+        Release blog
+      </Link>{' '}
+      |{' '}
+      <Link
+        className={'text-blue-600 hover:text-black dark:text-blue-500'}
+        href="https://github.com/transitmatters/t-performance-dash"
+      >
+        Source code
+      </Link>{' '}
+      |{' '}
+      <Link className={'text-blue-600 hover:text-black dark:text-blue-500'} href="/opensource">
+        Attributions
+      </Link>{' '}
+      |{' '}
+      <Link
+        className={'text-blue-600 hover:text-black dark:text-blue-500'}
+        href="mailto:labs@transitmatters.org?subject=[Data%20Dashboard%20Feedback]%20-%20"
+      >
+        🚀 Send feedback
+      </Link>
+    </footer>
+  );
+};
