faf2d2b2d95d24bf3436d95abaac2d240713a53b
Patrick Cleary <patcleary11@gmail.com>
2023-03-28 22:17:20 -0400

    Use Headless Tabs and some styling

--- modules/navigation/desktop/DataPageTabs.tsx
+++ modules/navigation/desktop/DataPageTabs.tsx
@@ -1,46 +1,64 @@
 import classNames from 'classnames';
 import React from 'react';
 import { pickBy } from 'lodash';
-import { useDelimitatedRoute } from '../../../common/utils/router';
+import {
+  getBusRouteSelectionItemHref,
+  getLineSelectionItemHref,
+  useDelimitatedRoute,
+} from '../../../common/utils/router';
 import { DATA_PAGES } from '../../../common/constants/datapages';
-import { ActiveLink } from '../../../common/components/general/ActiveLink';
+import { Tab } from '@headlessui/react';
+import { useRouter } from 'next/router';
+import { LINE_OBJECTS } from '../../../common/constants/lines';
+import { lineColorBackground, lineColorBorder } from '../../../common/styles/general';
+import { buttonHighlightConfig } from '../../../common/components/inputs/styles/inputStyle';
 
 export const DataPageTabs = () => {
-  const { datapage, line, linePath, query } = useDelimitatedRoute();
+  const route = useDelimitatedRoute();
+  const { query, linePath, line, datapage } = route;
+  const router = useRouter();
 
   return (
     <div className="md:mt-4">
       <div className="hidden sm:block">
-        <nav className="-mb-px flex space-x-8">
-          {Object.entries(DATA_PAGES).map(
-            ([key, page]) =>
-              line &&
-              page.lines.includes(line) && (
-                <ActiveLink
-                  key={key}
-                  href={{
-                    pathname: `/${linePath}${page.href}`,
-                    query: pickBy(query, (attr) => attr !== undefined),
-                  }}
-                  activeClassName={'border-gray-500 text-black'}
-                  lambda={() => {
-                    return key === datapage;
-                  }}
-                >
-                  <div
-                    className={classNames(
-                      `cursor-pointer select-none whitespace-nowrap border-b-2 ${
-                        datapage !== key &&
-                        'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-                      }  whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium`
+        <Tab.Group
+          onChange={(value) => {
+            const [key, page] = Object.entries(DATA_PAGES)[value];
+            router.push({
+              pathname: `/${linePath}${page.href}`,
+              query: pickBy(query, (attr) => attr !== undefined),
+            });
+          }}
+        >
+          <Tab.List className="flex">
+            {Object.entries(DATA_PAGES).map(
+              ([key, page]) =>
+                line && (
+                  <Tab key={key} disabled={!page.lines.includes(line)}>
+                    {({ selected }) => (
+                      <div
+                        title={
+                          page.lines.includes(line)
+                            ? DATA_PAGES[key].name
+                            : `${DATA_PAGES[key].name} not available for ${linePath}`
+                        }
+                        className={classNames(
+                          `select-none whitespace-nowrap border-b-2 px-4 pb-4 text-sm font-medium`,
+                          page.lines.includes(line)
+                            ? 'cursor-pointer hover:border-gray-300 hover:text-gray-700'
+                            : 'cursor-select text-opacity-40',
+                          selected && lineColorBorder[line ?? 'DEFAULT'],
+                          datapage !== key && 'border-transparent text-gray-600 '
+                        )}
+                      >
+                        {page.name}
+                      </div>
                     )}
-                  >
-                    {page.name}
-                  </div>
-                </ActiveLink>
-              )
-          )}
-        </nav>
+                  </Tab>
+                )
+            )}
+          </Tab.List>
+        </Tab.Group>
       </div>
     </div>
   );
