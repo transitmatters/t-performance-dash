9690e67c747dd3a92439dbbcf64491471aef2ff2
Patrick Cleary <patcleary11@gmail.com>
2023-03-30 14:32:05 -0400

    Add line health date range (not functional)
    This reverts commit 029a0f5171db50bf94b478a5777f8104c5284019.

---
+++ modules/dashboard/LineHealth.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { ButtonGroup } from '../../common/components/general/ButtonGroup';
+import type { TimeRange } from '../../common/types/inputs';
+import { TimeRangeNames } from '../../common/types/inputs';
+import { useDelimitatedRoute } from '../../common/utils/router';
+import { DwellsWidget } from '../dwells/DwellsWidget';
+import { HeadwaysWidget } from '../headways/HeadwaysWidget';
+import { RidershipWidget } from '../ridership/RidershipWidget';
+import SlowZonesWidget from '../slowzones/SlowZonesWidget';
+import { TravelTimesWidget } from '../traveltimes/TravelTimesWidget';
+
+export const LineHealth = () => {
+  const [timeRange, setTimeRange] = useState<TimeRange>('week');
+  const { tab } = useDelimitatedRoute();
+
+  return (
+    <div>
+      <div className="flex w-full flex-col justify-between sm:flex-row">
+        <h1 className="text-xl">Line Health</h1>
+        <ButtonGroup pressFunction={setTimeRange} options={Object.entries(TimeRangeNames)} />
+      </div>
+      <hr className="my-2 h-[2px] border-0 border-b border-white bg-gray-400" />
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-2">
+        {tab === 'Subway' && <SlowZonesWidget />}
+        <TravelTimesWidget />
+        <HeadwaysWidget />
+        {tab === 'Subway' && <DwellsWidget />}
+        <RidershipWidget />
+      </div>
+    </div>
+  );
+};
