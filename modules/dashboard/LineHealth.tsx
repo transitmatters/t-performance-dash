466e9caeddb8b420ccc7ce925ba008d2981ee1be
Patrick Cleary <patcleary11@gmail.com>
2023-04-04 14:46:01 -0400

    Remove Station-specific widgets from overview

--- modules/dashboard/LineHealth.tsx
+++ modules/dashboard/LineHealth.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import type { TimeRange } from '../../common/types/inputs';
 import { useDelimitatedRoute } from '../../common/utils/router';
-import { DwellsWidget } from '../dwells/DwellsWidget';
-import { HeadwaysWidget } from '../headways/HeadwaysWidget';
 import { RidershipWidget } from '../ridership/RidershipWidget';
 import SlowZonesWidget from '../slowzones/SlowZonesWidget';
-import { TravelTimesWidget } from '../traveltimes/TravelTimesWidget';
 
 export const LineHealth = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('week');
@@ -15,9 +12,6 @@ export const LineHealth = () => {
     <div>
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-2">
         {tab === 'Subway' && <SlowZonesWidget />}
-        <TravelTimesWidget />
-        <HeadwaysWidget />
-        {tab === 'Subway' && <DwellsWidget />}
         <RidershipWidget />
       </div>
     </div>
