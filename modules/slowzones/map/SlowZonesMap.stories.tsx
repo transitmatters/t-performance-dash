2d6b53db702e1faa8fb998602ae124dbc6fea814
Ian Reynolds <idreyn@gmail.com>
2023-04-04 00:43:16 -0400

    remove iffy import from stories

--- modules/slowzones/map/SlowZonesMap.stories.tsx
+++ modules/slowzones/map/SlowZonesMap.stories.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import type { SlowZoneResponse } from '../../../common/types/dataPoints';
-import slowZonesResponses from '../../../public/static/slowzones/all_slow.json';
-
 import SlowZonesMap from './SlowZonesMap';
 
 export default {
@@ -10,19 +8,36 @@ export default {
   component: SlowZonesMap,
 };
 
+const slowZonesResponses: SlowZoneResponse[] = [
+  {
+    color: 'Red',
+    fr_id: '70061',
+    to_id: '70063',
+    start: '2023-03-12T00:00:00Z',
+    end: '2023-04-01T00:00:00Z',
+    delay: 42.5,
+    mean_metric: 172.929,
+    baseline: 135.0,
+    duration: 20,
+  },
+  {
+    color: 'Red',
+    fr_id: '70063',
+    to_id: '70061',
+    start: '2023-03-12T00:00:00Z',
+    end: '2023-04-01T00:00:00Z',
+    delay: 90,
+    mean_metric: 172.929,
+    baseline: 135.0,
+    duration: 20,
+  },
+];
+
 export const Primary = () => {
   return (
     <>
-      <SlowZonesMap
-        lineName="Red"
-        slowZones={slowZonesResponses as SlowZoneResponse[]}
-        direction="horizontal"
-      />
-      <SlowZonesMap
-        lineName="Red"
-        slowZones={slowZonesResponses as SlowZoneResponse[]}
-        direction="vertical"
-      />
+      <SlowZonesMap lineName="Red" slowZones={slowZonesResponses} direction="horizontal" />
+      <SlowZonesMap lineName="Red" slowZones={slowZonesResponses} direction="vertical" />
     </>
   );
 };
