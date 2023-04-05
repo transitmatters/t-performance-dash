1c1323d1ac08594aef95a357d9e67fda3f09ced3
Patrick Cleary <patcleary11@gmail.com>
2023-04-05 12:53:49 -0400

    slightly lighter bg

--- common/components/widgets/internal/CompWidget.tsx
+++ common/components/widgets/internal/CompWidget.tsx
@@ -15,7 +15,7 @@ interface CompWidgetProps {
 
 export const CompWidget: React.FC<CompWidgetProps> = ({ value, text }) => {
   return (
-    <div className="flex w-full flex-row items-baseline rounded-md border-black border-opacity-30 bg-black bg-opacity-20 px-4 py-2">
+    <div className="flex w-full flex-row items-baseline rounded-md border-black border-opacity-30 bg-black bg-opacity-[15%] px-4 py-2">
       <div className="flex flex-col gap-y-1 text-white text-opacity-95">
         <div className="flex flex-row items-baseline">
           <BigDelta delta={value} sentimentDirection="positiveOnIncrease" />
