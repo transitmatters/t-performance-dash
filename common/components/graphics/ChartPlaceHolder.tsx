22c812fd8a66357705a377dd2db72597970c090a
Devin Matte <devinmatte@gmail.com>
2023-04-05 11:39:57 -0400

    Cleaning up errors slightly (#443)
    * Cleaning up errors slightly
    
    * Center error message in block
    
    * Keeping headers

--- common/components/graphics/ChartPlaceHolder.tsx
+++ common/components/graphics/ChartPlaceHolder.tsx
@@ -1,5 +1,6 @@
 import type { UseQueryResult } from '@tanstack/react-query';
 import React from 'react';
+import { ErrorNotice } from '../notices/ErrorNotice';
 import { LoadingSpinner } from './LoadingSpinner';
 
 interface ChartPlaceHolder {
@@ -9,7 +10,7 @@ interface ChartPlaceHolder {
 export const ChartPlaceHolder: React.FC<ChartPlaceHolder> = ({ query }) => {
   return (
     <div className="relative flex h-60 w-full items-center justify-center">
-      {query.isError ? <p>An error has occurred.</p> : <LoadingSpinner />}
+      {query.isError ? <ErrorNotice /> : <LoadingSpinner />}
     </div>
   );
 };
---
+++ common/components/notices/ErrorNotice.tsx
@@ -0,0 +1,30 @@
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import React from 'react';
+import { useDelimitatedRoute } from '../../utils/router';
+import { mbtaTextConfig } from '../inputs/styles/tailwind';
+
+interface ErrorNoticeProps {
+  isWidget?: boolean;
+}
+
+export const ErrorNotice: React.FC<ErrorNoticeProps> = ({ isWidget }) => {
+  const { line } = useDelimitatedRoute();
+
+  return (
+    <div
+      className={classNames(
+        'flex h-full flex-col content-center items-center justify-center rounded-lg text-center',
+        isWidget ? 'bg-white p-2 shadow-dataBox' : ''
+      )}
+    >
+      <FontAwesomeIcon
+        size={'3x'}
+        icon={faTriangleExclamation}
+        className={line && classNames(mbtaTextConfig[line])}
+      />
+      <>Uh oh... error</>
+    </div>
+  );
+};
