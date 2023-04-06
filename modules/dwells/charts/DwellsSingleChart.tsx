4ba34355f6ef2270f82be8cbc074262bce2cb474
Devin Matte <devinmatte@gmail.com>
2023-04-06 12:58:26 -0400

    Adding download buttons, and cleaning up (#448)
    * Adding download button, and cleaning up
    
    * isHomescreen

--- modules/dwells/charts/DwellsSingleChart.tsx
+++ modules/dwells/charts/DwellsSingleChart.tsx
@@ -11,14 +11,14 @@ interface DwellsSingleChartProps {
   dwells: UseQueryResult<SingleDayDataPoint[]>;
   toStation: Station | undefined;
   fromStation: Station | undefined;
-  homescreen?: boolean;
+  isHomescreen?: boolean;
 }
 
 export const DwellsSingleChart: React.FC<DwellsSingleChartProps> = ({
   dwells,
   toStation,
   fromStation,
-  homescreen = false,
+  isHomescreen = false,
 }) => {
   const {
     linePath,
@@ -44,10 +44,19 @@ export const DwellsSingleChart: React.FC<DwellsSingleChartProps> = ({
         location={locationDetails(fromStation, toStation, lineShort)}
         fname={'dwells'}
         showLegend={false}
-        homescreen={homescreen}
+        isHomescreen={isHomescreen}
       />
     );
-  }, [dwells.data, fromStation, isLoading, linePath, lineShort, startDate, toStation, homescreen]);
+  }, [
+    dwells.data,
+    fromStation,
+    isLoading,
+    linePath,
+    lineShort,
+    startDate,
+    toStation,
+    isHomescreen,
+  ]);
 
   return chart;
 };
