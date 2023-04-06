6d6b29aea4e93ee3c07ef803a8e353ea5fce3bb0
Patrick Cleary <patcleary11@gmail.com>
2023-04-06 13:47:48 -0400

    Change Date Picker to only be range OR Single day

--- common/components/inputs/DateSelection/DatePickers.tsx
+++ common/components/inputs/DateSelection/DatePickers.tsx
@@ -2,22 +2,20 @@ import dayjs from 'dayjs';
 import type { SetStateAction } from 'react';
 import React from 'react';
 import classNames from 'classnames';
-import { faClose, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Flatpickr from 'react-flatpickr';
 import 'flatpickr/dist/themes/light.css';
 import { useDelimitatedRoute, useUpdateQuery } from '../../../utils/router';
-import { DATE_PICKER_PRESETS, FLAT_PICKER_OPTIONS, TODAY_STRING } from '../../../constants/dates';
+import { FLAT_PICKER_OPTIONS } from '../../../constants/dates';
 import { buttonHighlightFocus } from '../../../styles/general';
-import { RangeButton } from './RangeButton';
-import type { DateSelectionInput } from './types/DateSelectionTypes';
 
 interface DatePickerProps {
-  config: DateSelectionInput;
-  setConfig: React.Dispatch<SetStateAction<DateSelectionInput>>;
+  range: boolean;
+  setSelection: React.Dispatch<SetStateAction<number | undefined>>;
 }
 
-export const DatePickers: React.FC<DatePickerProps> = ({ config, setConfig }) => {
+export const DatePickers: React.FC<DatePickerProps> = ({ range, setSelection }) => {
   const updateQueryParams = useUpdateQuery();
   const { query, line, tab } = useDelimitatedRoute();
   const { startDate, endDate } = query;
@@ -27,46 +25,31 @@ export const DatePickers: React.FC<DatePickerProps> = ({ config, setConfig }) =>
   const handleEndDateChange = (date: string) => {
     const updatedDate = dayjs(date);
     if (updatedDate.isSame(startDateObject)) {
-      // Convert to single day mode when startDate == endDate
-      handleRangeToggle();
+      // TODO: set to undefined?
       return;
     }
     if (updatedDate.isBefore(startDateObject)) {
       // Swap start and end if new end < startDate
-      updateQueryParams({ startDate: date, endDate: startDate }, config.range);
+      updateQueryParams({ startDate: date, endDate: startDate }, range);
     } else {
-      updateQueryParams({ startDate: startDate, endDate: date }, config.range);
+      updateQueryParams({ startDate: startDate, endDate: date }, range);
     }
-    setConfig({ ...config, selection: undefined });
+    setSelection(undefined);
   };
 
   const handleStartDateChange = (date: string) => {
     const updatedDate = dayjs(date);
     if (updatedDate.isSame(endDateObject)) {
-      // Convert to single day mode when startDate == endDate
-      handleRangeToggle();
+      // TODO: set to undefined?
       return;
     }
     if (updatedDate.isAfter(endDateObject)) {
       // Swap start and end if new start > endDate
-      updateQueryParams({ startDate: endDate, endDate: date }, config.range);
+      updateQueryParams({ startDate: endDate, endDate: date }, range);
     } else {
-      updateQueryParams({ startDate: date, endDate: endDate }, config.range);
+      updateQueryParams({ startDate: date, endDate: endDate }, range);
     }
-    setConfig({ ...config, selection: undefined });
-  };
-
-  // Handle swapping between single day and aggregates.
-  const handleRangeToggle = () => {
-    if (config.range) {
-      updateQueryParams({ startDate: startDate }, !config.range);
-    } else if (startDate === TODAY_STRING) {
-      // If start date is today -> set range to past week. This prevents bugs with startDate === endDate
-      updateQueryParams(DATE_PICKER_PRESETS.range[0].input, !config.range);
-    } else {
-      updateQueryParams({ startDate: startDate }, !config.range);
-    }
-    setConfig({ selection: undefined, range: !config.range });
+    setSelection(undefined);
   };
 
   return (
@@ -86,7 +69,7 @@ export const DatePickers: React.FC<DatePickerProps> = ({ config, setConfig }) =>
             handleStartDateChange(currentDateString);
           }}
         />
-        {config.range ? (
+        {range ? (
           <>
             <div
               className={classNames('flex items-center self-stretch bg-white text-sm text-black')}
@@ -110,13 +93,6 @@ export const DatePickers: React.FC<DatePickerProps> = ({ config, setConfig }) =>
             />
           </>
         ) : null}
-        <RangeButton onClick={handleRangeToggle}>
-          {config.range ? (
-            <FontAwesomeIcon icon={faClose} className="h-4 w-4 text-black" />
-          ) : (
-            <FontAwesomeIcon icon={faArrowRight} className="h-4 w-4 text-black" />
-          )}
-        </RangeButton>
       </div>
     </div>
   );
