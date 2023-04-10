b2c26565ff00585e6e18d736253fb21395ebf709
Devin Matte <devinmatte@gmail.com>
2023-04-10 14:28:55 -0400

    Making alerts stale at 1 minute

--- common/api/hooks/alerts.ts
+++ common/api/hooks/alerts.ts
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchAlerts } from '../alerts';
 import type { LineShort } from '../../types/lines';
+import { ONE_MINUTE } from '../../constants/time';
 
 export const useAlertsData = (route: LineShort, busRoute?: string) => {
-  return useQuery(['alerts', route, busRoute], () => fetchAlerts(route, busRoute));
+  return useQuery(['alerts', route, busRoute], () => fetchAlerts(route, busRoute), {
+    staleTime: ONE_MINUTE,
+  });
 };
