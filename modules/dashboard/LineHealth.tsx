85b998d64cf0bca0c3fc971b70309c7347d6b67a
Patrick Cleary <patcleary11@gmail.com>
2023-04-10 13:50:26 -0400

    add back date selector to lineHealth page

--- modules/dashboard/LineHealth.tsx
+++ modules/dashboard/LineHealth.tsx
@@ -6,12 +6,17 @@ import { RidershipWidget } from '../ridership/RidershipWidget';
 import SlowZonesWidget from '../slowzones/SlowZonesWidget';
 import { SpeedWidget } from '../speed/SpeedWidget';
 import { TravelTimesWidget } from '../traveltimes/TravelTimesWidget';
+import { OverviewDateSelection } from '../../common/components/inputs/DateSelection/OverviewDateSelection';
 
 export const LineHealth = () => {
   const { tab, line } = useDelimitatedRoute();
 
   return (
     <div>
+      <div className="flex w-full flex-col justify-between sm:flex-row">
+        <h1 className="text-xl">Line Health</h1>
+        <OverviewDateSelection />
+      </div>
       <hr className="my-2 h-[2px] border-0 border-b border-white bg-gray-400" />
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-2">
         {tab === 'Subway' && line !== 'GL' && <SpeedWidget />}
