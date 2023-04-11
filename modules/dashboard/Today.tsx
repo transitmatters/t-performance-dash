9497567b0d83223b814b15df923f7708bb57067f
Patrick Cleary <patcleary11@gmail.com>
2023-04-10 21:46:04 -0400

    lint fix

--- modules/dashboard/Today.tsx
+++ modules/dashboard/Today.tsx
@@ -9,13 +9,15 @@ import { WidgetTitle } from './WidgetTitle';
 export default function TodaysCommute() {
   const { tab, line, lineShort } = useDelimitatedRoute();
   const allSlow = useQuery(['allSlow'], fetchAllSlow);
+  const canShowSlowZonesMap = lineShort === 'Red' || lineShort === 'Blue' || lineShort === 'Orange';
+
   return (
     <div className="flex flex-col gap-y-4">
       <div className="flex flex-col gap-x-4 gap-y-4 xl:flex-row">
         <Alerts />
         {tab === 'Subway' && line !== 'GL' && <Speed />}
       </div>
-      {tab === 'Subway' && line !== 'GL' && allSlow.data && (
+      {tab === 'Subway' && canShowSlowZonesMap && allSlow.data && (
         <div className="h-full rounded-lg border-design-lightGrey bg-white p-2 shadow-dataBox">
           <WidgetTitle title="Slow Zones" />
           <SlowZonesMap
