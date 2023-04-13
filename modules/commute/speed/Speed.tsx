3b430f1dff9b03a7b4acc68a3626dc5b6c68146d
Devin Matte <devinmatte@gmail.com>
2023-04-13 14:01:52 -0400

    Show shuttling when line is being shuttled

--- modules/commute/speed/Speed.tsx
+++ modules/commute/speed/Speed.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import dayjs from 'dayjs';
+import { Tooltip } from 'flowbite-react';
 import { useDelimitatedRoute } from '../../../common/utils/router';
 import { lineColorBackground } from '../../../common/styles/general';
 import { useSpeedData } from '../../../common/api/hooks/speed';
@@ -9,10 +10,15 @@ import { CompWidget } from '../../../common/components/widgets/internal/CompWidg
 import { DATE_FORMAT, OVERVIEW_OPTIONS } from '../../../common/constants/dates';
 import { ChartPlaceHolder } from '../../../common/components/graphics/ChartPlaceHolder';
 import { MINIMUMS } from '../../speed/constants/speeds';
+import { useAlertsData } from '../../../common/api/hooks/alerts';
+import ShuttleIcon from '../../../public/Icons/ShuttleIcon.svg';
+import { AlertEffect } from '../../../common/types/alerts';
+import { ShuttleAlert } from '../alerts/ShuttleAlert';
+import { getRelevantAlerts } from '../alerts/AlertBox';
 import { calculateCommuteSpeedWidgetValues } from './utils/utils';
 
 export const Speed: React.FC = () => {
-  const { line } = useDelimitatedRoute();
+  const { line, lineShort } = useDelimitatedRoute();
   const today = dayjs().format(DATE_FORMAT);
   const { startDate } = OVERVIEW_OPTIONS.week;
 
@@ -20,6 +26,12 @@ export const Speed: React.FC = () => {
   const weekly = useSpeedData({ line, start_date: startDate, end_date: today, agg: 'daily' });
 
   const speedReady = line && weekly.data && speed.data && !speed.isLoading && !speed.isError;
+
+  // Check if shuttling
+  const alerts = useAlertsData(lineShort);
+  const shuttlingAlert = alerts.data
+    ? getRelevantAlerts(alerts.data, 'current').find((alert) => alert?.type === AlertEffect.SHUTTLE)
+    : undefined;
 
   const divStyle = classNames(
     'items-center justify-center rounded-lg py-4 px-6 text-center sm:w-1/2 xl:w-1/3 text-opacity-95 text-white',
@@ -53,32 +65,50 @@ export const Speed: React.FC = () => {
           info={`Speed is how quickly a train traverses the entire line, including time spent at stations.`}
         />
       </div>
-      <div className="mt-2 flex flex-row items-baseline justify-center gap-x-1">
-        <p className={classNames('text-5xl font-semibold')}>
-          {isNaN(MPH) ? '...' : MPH.toFixed(1)}
-        </p>
-        <p className="text-2xl text-opacity-90">mph</p>
-      </div>
-      <div className="pt-4">
-        <div className="mt-2 flex flex-col gap-x-2 gap-y-2">
-          <CompWidget
-            value={weeklyComp}
-            text={
-              <p>
-                Than <b>7 day</b> average
-              </p>
-            }
-          />
-          <CompWidget
-            value={peakComp}
-            text={
-              <p>
-                Than <b>system peak</b> ({MINIMUMS[line ?? 'DEFAULT'].date})
-              </p>
-            }
-          />
+      {shuttlingAlert ? (
+        <div className="mt-2 flex flex-col justify-center gap-x-1">
+          <div className={'self-center'}>
+            <Tooltip
+              content={
+                <p className="max-w-xs">
+                  {"We can't calculate train speed during line shuttling (even partial shuttling)"}
+                </p>
+              }
+            >
+              <ShuttleIcon className="m-3 h-12 w-12 self-center" aria-hidden="true" />
+            </Tooltip>
+          </div>
+          <ShuttleAlert alert={shuttlingAlert} lineShort={lineShort} type={'current'} />
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="mt-2 flex flex-row items-baseline justify-center gap-x-1">
+            <p className={classNames('text-5xl font-semibold')}>
+              {isNaN(MPH) ? '...' : MPH.toFixed(1)}
+            </p>
+          </div>
+          <div className="pt-4">
+            <div className="mt-2 flex flex-col gap-x-2 gap-y-2">
+              <CompWidget
+                value={weeklyComp}
+                text={
+                  <p>
+                    Than <b>7 day</b> average
+                  </p>
+                }
+              />
+              <CompWidget
+                value={peakComp}
+                text={
+                  <p>
+                    Than <b>system peak</b> ({MINIMUMS[line ?? 'DEFAULT'].date})
+                  </p>
+                }
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
