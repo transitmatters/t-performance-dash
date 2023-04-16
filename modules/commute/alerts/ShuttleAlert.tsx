b27cdca3787b7a92cfde7e68b1d1d7363db01080
Devin Matte <devinmatte@gmail.com>
2023-04-16 12:32:43 -0400

    Station can sometimes be undefined (#492)

--- modules/commute/alerts/ShuttleAlert.tsx
+++ modules/commute/alerts/ShuttleAlert.tsx
@@ -16,9 +16,9 @@ export const ShuttleAlert: React.FC<ShuttleAlertProps> = ({ alert, lineShort, ty
   return (
     <AlertBoxInner header={alert.header} Icon={ShuttleIcon} type={type} alert={alert}>
       <p className="mr-1 ">Shuttling</p>
-      <p className="font-bold">{min.stop_name}</p>
+      <p className="font-bold">{min?.stop_name}</p>
       <BetweenArrow className="mx-2 h-4 w-4" />
-      <p className="font-bold">{max.stop_name}</p>
+      <p className="font-bold">{max?.stop_name}</p>
     </AlertBoxInner>
   );
 };
