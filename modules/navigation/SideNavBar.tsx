c11b8e261b7bf3029a2d0aa2d6e8b32dcacb6b62
Patrick Cleary <patcleary11@gmail.com>
2023-04-29 22:15:40 -0700

    Hacky mobile sidebar changes. Should redo

--- modules/navigation/SideNavBar.tsx
+++ modules/navigation/SideNavBar.tsx
@@ -12,63 +12,17 @@ export const SideNavBar = () => {
     <>
       <div className="bg-tm-grey text-gray-300">
         <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-40 md:hidden" onClose={setSidebarOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed bottom-0 right-0 top-0 bg-gray-600 bg-opacity-75" />
-            </Transition.Child>
+          <Dialog as="div" className="relative z-40 w-full md:hidden" onClose={setSidebarOpen}>
+            <div className="fixed bottom-0 right-0 top-0 bg-gray-600 bg-opacity-75" />
 
-            <div className="fixed bottom-0 right-0 top-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-tm-grey">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute left-0 top-0 -mr-12 pt-2">
-                      <button
-                        type="button"
-                        className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                        onClick={() => setSidebarOpen(false)}
-                      >
-                        <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  <div className="h-0 flex-1 overflow-y-auto px-4 pb-4 pt-5">
-                    <div className="flex flex-shrink-0 ">
-                      <TmLogoSvg className="h-7 w-auto sm:h-12" alt="TransitMatters Logo" />
-                    </div>
-                    <div className="mt-5 h-0 flex-1 text-white">
-                      <SideNavigation setSidebarOpen={setSidebarOpen} />
-                    </div>
+            <div className="fixed bottom-0 right-0 top-[61px] z-40 flex w-full md:top-0">
+              <Dialog.Panel className="relative flex w-full flex-1 flex-col bg-tm-grey">
+                <div className="h-0 flex-1 overflow-y-auto px-4 pb-4 pt-5">
+                  <div className="mt-5 h-0 flex-1 text-white">
+                    <SideNavigation setSidebarOpen={setSidebarOpen} />
                   </div>
-                </Dialog.Panel>
-              </Transition.Child>
-              <div className="w-14 flex-shrink-0" aria-hidden="true">
-                {/* Force sidebar to shrink to fit close icon */}
-              </div>
+                </div>
+              </Dialog.Panel>
             </div>
           </Dialog>
         </Transition.Root>
@@ -93,10 +47,14 @@ export const SideNavBar = () => {
             <button
               type="button"
               className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-400"
-              onClick={() => setSidebarOpen(true)}
+              onClick={() => setSidebarOpen(!sidebarOpen)}
             >
-              <span className="sr-only">Open sidebar</span>
-              <Bars3Icon className="h-8 w-8 text-stone-100" aria-hidden="true" />
+              <span className="sr-only">{sidebarOpen ? 'Open Sidebar' : 'Close Sidebar'}</span>
+              {sidebarOpen ? (
+                <XMarkIcon className="h-8 w-8 text-stone-100" aria-hidden="true" />
+              ) : (
+                <Bars3Icon className="h-8 w-8 text-stone-100" aria-hidden="true" />
+              )}
             </button>
           </div>
         </div>
