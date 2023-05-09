ca17a8d2c138fe6b04672e02585f5e73b87b7b5c
Patrick Cleary <patrickdaviscleary@gmail.com>
2023-05-09 16:46:08 -0700

    V4/chart annotations (#549)
    * shuttling blocks and baseline

--- modules/service/ServiceGraph.tsx
+++ modules/service/ServiceGraph.tsx
@@ -15,15 +15,18 @@ import {
 import 'chartjs-adapter-date-fns';
 import { enUS } from 'date-fns/locale';
 import pattern from 'patternomaly';
+import Annotation from 'chartjs-plugin-annotation';
 
 import ChartjsPluginWatermark from 'chartjs-plugin-watermark';
 import { useDelimitatedRoute } from '../../common/utils/router';
-import { COLORS, LINE_COLORS } from '../../common/constants/colors';
+import { CHART_COLORS, COLORS, LINE_COLORS } from '../../common/constants/colors';
 import type { SpeedDataPoint, TripCounts } from '../../common/types/dataPoints';
 import { drawSimpleTitle } from '../../common/components/charts/Title';
 import { hexWithAlpha } from '../../common/utils/general';
 import type { ParamsType } from '../speed/constants/speeds';
+import { PEAK_SCHEDULED_SERVICE } from '../../common/constants/service';
 import { useBreakpoint } from '../../common/hooks/useBreakpoint';
+import { getShuttlingBlockAnnotations } from './utils/graphUtils';
 
 ChartJS.register(
   CategoryScale,
@@ -31,6 +34,7 @@ ChartJS.register(
   LinearScale,
   PointElement,
   LineElement,
+  Annotation,
   Filler,
   Title,
   Tooltip,
@@ -63,7 +67,7 @@ export const ServiceGraph: React.FC<ServiceGraphProps> = ({
 
   const labels = data.map((point) => point.date);
   const lineColor = LINE_COLORS[line ?? 'default'];
-
+  const shuttlingBlocks = getShuttlingBlockAnnotations(data);
   return (
     <Line
       id={'Service'}
@@ -98,6 +102,12 @@ export const ServiceGraph: React.FC<ServiceGraphProps> = ({
               data[index]?.value > 0 && count ? count / 2 : Number.NaN
             ),
             backgroundColor: pattern.draw('diagonal', '#FFFFFF', lineColor, 5),
+          },
+          {
+            // This null dataset produces the entry in the legend for the baseline annotation.
+            label: `Baseline (${PEAK_SCHEDULED_SERVICE[line ?? 'DEFAULT']})`,
+            backgroundColor: CHART_COLORS.ANNOTATIONS,
+            data: null,
           },
         ],
       }}
@@ -141,6 +151,20 @@ export const ServiceGraph: React.FC<ServiceGraphProps> = ({
             // empty title to set font and leave room for drawTitle fn
             display: showTitle,
             text: '',
+          },
+          annotation: {
+            // Add your annotations here
+            annotations: [
+              {
+                type: 'line',
+                yMin: PEAK_SCHEDULED_SERVICE[line ?? 'DEFAULT'],
+                yMax: PEAK_SCHEDULED_SERVICE[line ?? 'DEFAULT'],
+                borderColor: CHART_COLORS.ANNOTATIONS,
+                display: (ctx) => ctx.chart.isDatasetVisible(2),
+                borderWidth: 2,
+              },
+              ...shuttlingBlocks,
+            ],
           },
         },
         scales: {
@@ -204,6 +228,7 @@ export const ServiceGraph: React.FC<ServiceGraphProps> = ({
             if (showTitle) drawSimpleTitle(`Daily round trips`, chart);
           },
         },
+        Annotation,
       ]}
     />
   );
