d535f5a709d60fc6fe658644821d8559e66a3e39
Kevin Moses <kmoses@Kevins-MacBook-Pro.local>
2023-05-15 20:12:09 -0400

    Add types

--- common/types/facilities.ts
+++ common/types/facilities.ts
@@ -1,6 +1,27 @@
+export interface FacilityProperty {
+  value: string;
+  name: string;
+}
+
 export interface Facility {
+  type: string;
+  relationships: {
+    stop: {
+      data: {
+        type: 'stop';
+        id: string;
+      };
+    };
+  };
   id: string;
-  type: 'facility';
+  attributes: {
+    type: 'ELEVATOR' | 'ESCALATOR';
+    short_name: string;
+    properties: FacilityProperty[];
+    longitude: number;
+    long_name: string;
+    latitude: number;
+  };
 }
 
 export interface FacilitiesResponse {
