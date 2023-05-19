53b785b785b0739bd17345c3b82f8ebd42b10092
Patrick Cleary <patrickdaviscleary@gmail.com>
2023-05-19 09:13:44 -0400

    Add dates to date placeholder (#585)

--- modules/dashboard/WidgetTitle.tsx
+++ modules/dashboard/WidgetTitle.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import type { Location } from '../../common/types/charts';
 import type { Line } from '../../common/types/lines';
 import { useBreakpoint } from '../../common/hooks/useBreakpoint';
+import { useDelimitatedRoute } from '../../common/utils/router';
+import { getSelectedDates } from '../../common/state/utils/dashboardUtils';
 import { LocationTitle } from './LocationTitle';
 
 interface WidgetTitle {
@@ -21,6 +23,12 @@ export const WidgetTitle: React.FC<WidgetTitle> = ({
   line,
 }) => {
   const isMobile = !useBreakpoint('md');
+  const { query } = useDelimitatedRoute();
+  const date = getSelectedDates({
+    startDate: query.startDate,
+    endDate: query.endDate,
+    view: query.view,
+  });
   return (
     <div className="flex w-full flex-col items-baseline justify-between gap-x-4 p-2 text-xl md:flex-row">
       <div className="flex w-full flex-col md:w-auto ">
@@ -28,7 +36,7 @@ export const WidgetTitle: React.FC<WidgetTitle> = ({
           <h2 className={classNames('font-semibold', 'whitespace-nowrap text-stone-800')}>
             {title}
           </h2>
-          {isMobile && <p className="text-xs italic text-stone-700">{`Date Placeholder`}</p>}
+          {isMobile && <p className="text-xs italic text-stone-700">{date}</p>}
         </div>
         {subtitle && (
           <h2 className={classNames('whitespace-nowrap text-sm italic text-stone-600')}>
