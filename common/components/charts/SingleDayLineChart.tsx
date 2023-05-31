b849ba801eda20a98b024d49a437f6bf21862843
Patrick Cleary <patrickdaviscleary@gmail.com>
2023-05-31 14:20:06 -0400

    Use ChartDivs (#591)

--- common/components/charts/SingleDayLineChart.tsx
+++ common/components/charts/SingleDayLineChart.tsx
@@ -15,7 +15,6 @@ import 'chartjs-adapter-date-fns';
 import ChartjsPluginWatermark from 'chartjs-plugin-watermark';
 import { enUS } from 'date-fns/locale';
 import React, { useMemo, useRef } from 'react';
-import classNames from 'classnames';
 import type { DataPoint } from '../../types/dataPoints';
 import { CHART_COLORS, COLORS, LINE_COLORS } from '../../../common/constants/colors';
 import type { SingleDayLineProps } from '../../../common/types/charts';
@@ -26,6 +25,8 @@ import { useBreakpoint } from '../../hooks/useBreakpoint';
 import { watermarkLayout } from '../../constants/charts';
 import { writeError } from '../../utils/chartError';
 import { Legend as LegendView } from './Legend';
+import { ChartDiv } from './ChartDiv';
+import { ChartBorder } from './ChartBorder';
 
 ChartJS.register(
   CategoryScale,
@@ -93,12 +94,12 @@ export const SingleDayLineChart: React.FC<SingleDayLineProps> = ({
   const labels = useMemo(() => data.map((item) => item[pointField]), [data, pointField]);
   const { line } = useDelimitatedRoute();
   return (
-    <div className={classNames('relative flex w-full flex-col pr-2')}>
-      <div className="flex h-60 w-full flex-row">
+    <ChartBorder>
+      <ChartDiv isMobile={isMobile}>
         <Line
           id={chartId}
           ref={ref}
-          height={250}
+          height={isMobile ? 200 : 240}
           redraw={true}
           data={{
             labels,
@@ -222,7 +223,7 @@ export const SingleDayLineChart: React.FC<SingleDayLineProps> = ({
             },
           ]}
         />
-      </div>
+      </ChartDiv>
       <div className="flex flex-row items-end gap-4 pl-6 pr-2">
         {showLegend && benchmarkField ? <LegendView /> : <div className="w-full" />}
         {!isHomescreen && date && (
@@ -235,6 +236,6 @@ export const SingleDayLineChart: React.FC<SingleDayLineProps> = ({
           />
         )}
       </div>
-    </div>
+    </ChartBorder>
   );
 };
