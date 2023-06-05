c242d91db6339c4959559f02fdc537247434f12f
Patrick Cleary <patcleary11@gmail.com>
2023-06-05 13:59:25 -0400

    div for height

--- common/components/notices/NoDataNotice.tsx
+++ common/components/notices/NoDataNotice.tsx
@@ -20,17 +20,19 @@ export const NoDataNotice: React.FC<NoDataNoticeProps> = ({
 
   const color = !inverse && line ? mbtaTextConfig[line] : undefined;
   return (
-    <div
-      className={classNames(
-        'flex h-full flex-col content-center items-center justify-center rounded-lg text-center',
-        isWidget ? 'bg-white p-2 shadow-dataBox' : ''
-      )}
-    >
-      <FontAwesomeIcon size={'3x'} icon={faTriangleExclamation} className={color} />
-      <p className="text-stone-900">No data available.</p>
-      <p className="text-stone-600">
-        {isLineWide ? 'Try another date range.' : 'Try another station or date range.'}
-      </p>
+    <div className="relative flex h-60 w-full items-center justify-center">
+      <div
+        className={classNames(
+          'flex h-full flex-col content-center items-center justify-center rounded-lg text-center',
+          isWidget ? 'bg-white p-2 shadow-dataBox' : ''
+        )}
+      >
+        <FontAwesomeIcon size={'3x'} icon={faTriangleExclamation} className={color} />
+        <p className="text-stone-900">No data available.</p>
+        <p className="text-stone-600">
+          {isLineWide ? 'Try another date range.' : 'Try another station or date range.'}
+        </p>
+      </div>
     </div>
   );
 };
