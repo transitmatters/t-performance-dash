9d71fc011f338cf0d7fc2cb74e97b82e59193fbe
Patrick Cleary <patcleary11@gmail.com>
2023-06-06 14:29:19 -0400

    Landing Page Placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,6 @@
 'use client';
-import React from 'react';
-import Link from 'next/link';
-import { PageWrapper } from '../common/layouts/PageWrapper';
 import { Layout } from '../common/layouts/layoutTypes';
-export default function Home() {
-  return (
-    <PageWrapper>
-      <Link href="/red">
-        <div>
-          <p>Red Line</p>
-        </div>
-      </Link>
-      <Link href="/orange">
-        <div>
-          <p>Orange Line</p>
-        </div>
-      </Link>
-      <Link href="/green">
-        <div>
-          <p>Green Line</p>
-        </div>
-      </Link>
-      <Link href="/blue">
-        <div>
-          <p>Blue Line</p>
-        </div>
-      </Link>
-    </PageWrapper>
-  );
-}
+import { Landing } from '../modules/landing/Landing';
 
-Home.Layout = Layout.Landing;
+export default Landing;
+Landing.Layout = Layout.Landing;
