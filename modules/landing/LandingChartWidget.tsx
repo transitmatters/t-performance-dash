fdc4f678038a577a5bea94b9f819cf96b57ac832
Patrick Cleary <patcleary11@gmail.com>
2023-06-08 09:39:50 -0400

    style

--- modules/landing/LandingChartWidget.tsx
+++ modules/landing/LandingChartWidget.tsx
@@ -7,10 +7,10 @@ interface LandingChartWidgetProps {
 
 export const LandingChartWidget: React.FC<LandingChartWidgetProps> = ({ title, children }) => {
   return (
-    <div className="flex w-full flex-col justify-center gap-y-4 px-4 lg:flex-row lg:gap-x-12 lg:px-20 xl:gap-x-20">
+    <div className="flex w-full flex-col justify-center gap-y-4 px-4 md:px-8 lg:flex-row lg:gap-x-12 lg:px-20 xl:gap-x-20">
       <div className="flex max-w-lg flex-shrink flex-col gap-y-2">
-        <h2 className="text-7xl font-thin text-gray-400 lg:text-5xl xl:text-7xl">{title}</h2>
-        <p className="text-gray-100">
+        <h2 className="text-7xl font-thin text-stone-400 lg:text-5xl xl:text-7xl">{title}</h2>
+        <p className="text-stone-100">
           Explanation of how we calculate speed. Speed is important to riders because of X, Y, and
           Z. The baseline is DATE RANGE.
         </p>
