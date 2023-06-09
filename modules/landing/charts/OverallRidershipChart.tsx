cc99f49e6bdf8e761829418ee91269081ba10268
Patrick Cleary <patcleary11@gmail.com>
2023-06-09 10:23:07 -0400

    lint

--- modules/landing/charts/OverallRidershipChart.tsx
+++ modules/landing/charts/OverallRidershipChart.tsx
@@ -1,40 +1,8 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  TimeScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import 'chartjs-adapter-date-fns';
-import { enUS } from 'date-fns/locale';
-
-import { THREE_MONTHS_AGO_STRING, TODAY_STRING } from '../../../common/constants/dates';
-import { COLORS } from '../../../common/constants/colors';
 import type { RidershipCount } from '../../../common/types/dataPoints';
 import type { Line as LineType } from '../../../common/types/lines';
-import { SPEED_RANGE_PARAM_MAP } from '../../speed/constants/speeds';
 import { convertToRidershipDataset } from '../utils';
 import { LandingPageChart } from './LandingPageChart';
-
-ChartJS.register(
-  CategoryScale,
-  TimeScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Title,
-  Tooltip,
-  Legend
-);
 
 interface OverallRidershipChartProps {
   ridershipData: { line: LineType; data: RidershipCount[] }[];
