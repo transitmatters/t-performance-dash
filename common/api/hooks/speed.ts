a37a1f1c7d373dfc17270aceda4bb6a5e2163cdf
Patrick Cleary <patrickdaviscleary@gmail.com>
2023-06-09 11:41:03 -0400

    V4/landing page service graph (#636)
    service Data systemwide

--- common/api/hooks/speed.ts
+++ common/api/hooks/speed.ts
@@ -2,7 +2,7 @@ import { useQueries, useQuery } from '@tanstack/react-query';
 import { fetchSpeeds } from '../speed';
 import type { FetchSpeedsOptions } from '../../types/api';
 import { FIVE_MINUTES, ONE_HOUR } from '../../constants/time';
-import { ONE_YEAR_AGO_STRING, TODAY_STRING } from '../../constants/dates';
+import { THREE_MONTHS_AGO_STRING, TODAY_STRING } from '../../constants/dates';
 import { HEAVY_RAIL_LINES } from '../../types/lines';
 
 export const useSpeedData = (params: FetchSpeedsOptions, enabled?: boolean) => {
@@ -12,12 +12,15 @@ export const useSpeedData = (params: FetchSpeedsOptions, enabled?: boolean) => {
   });
 };
 
+/*
+ * useSpeedDataLanding queries the Speed data tables which include full-line trip times (value) and number of trips (count)
+ */
 export const useSpeedDataLanding = () => {
   return useQueries({
     queries: HEAVY_RAIL_LINES.map((line) => {
       const params: FetchSpeedsOptions = {
         line: line,
-        start_date: ONE_YEAR_AGO_STRING,
+        start_date: THREE_MONTHS_AGO_STRING,
         end_date: TODAY_STRING,
         agg: 'weekly',
       };
