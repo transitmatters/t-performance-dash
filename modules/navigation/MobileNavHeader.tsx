aa42f7055b3a4f80d37c8661868c5dbc9c1c29a3
Patrick Cleary <patcleary11@gmail.com>
2023-06-10 15:19:18 -0400

    Add home link to mobile nav dropdown.

--- modules/navigation/MobileNavHeader.tsx
+++ modules/navigation/MobileNavHeader.tsx
@@ -18,7 +18,25 @@ export const MobileNavHeader = () => {
           <Dialog as="div" className="relative z-40 w-full" onClose={setSidebarOpen}>
             <div className="fixed bottom-0 right-0 top-0 bg-gray-600 bg-opacity-75" />
 
-            <Dialog.Panel className="fixed bottom-0 left-0 right-0 top-12 flex w-full flex-col bg-tm-grey">
+            <Dialog.Panel className="fixed bottom-0 left-0 right-0 top-0 flex w-full flex-col bg-tm-grey">
+              <div className="top-0 z-10 flex flex-row items-center justify-between bg-tm-grey p-2">
+                <Link
+                  href="/"
+                  className="h-5 w-auto focus:outline-none"
+                  onClick={() => setSidebarOpen(false)}
+                >
+                  <TmLogoSvg className="mr-4 h-5 w-auto text-black" alt="TransitMatters Logo" />
+                </Link>
+
+                <button
+                  type="button"
+                  className="inline-flex h-8 w-8 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-400"
+                  onClick={() => setSidebarOpen(false)}
+                >
+                  <span className="sr-only">{'Close Sidebar'}</span>
+                  <XMarkIcon className="h-8 w-8 text-stone-100" aria-hidden="true" />
+                </button>
+              </div>
               <div className="h-0 flex-1 overflow-y-auto px-4 pt-5">
                 <div className="h-0 flex-1 text-white md:mt-5">
                   <SideNavigation setSidebarOpen={setSidebarOpen} />
@@ -42,14 +60,10 @@ export const MobileNavHeader = () => {
             <button
               type="button"
               className="inline-flex h-8 w-8 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-400"
-              onClick={() => setSidebarOpen(!sidebarOpen)}
+              onClick={() => setSidebarOpen(true)}
             >
-              <span className="sr-only">{sidebarOpen ? 'Open Sidebar' : 'Close Sidebar'}</span>
-              {sidebarOpen ? (
-                <XMarkIcon className="h-8 w-8 text-stone-100" aria-hidden="true" />
-              ) : (
-                <Bars3Icon className="h-8 w-8 text-stone-100" aria-hidden="true" />
-              )}
+              <span className="sr-only">{'Open Sidebar'}</span>
+              <Bars3Icon className="h-8 w-8 text-stone-100" aria-hidden="true" />
             </button>
           </div>
         </div>
