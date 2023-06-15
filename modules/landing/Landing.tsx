3becbe9ba21b8f5ba34ec9525dbbb9bcce9a6a92
Patrick Cleary <patcleary11@gmail.com>
2023-06-15 17:17:02 -0400

    subtitle

--- modules/landing/Landing.tsx
+++ modules/landing/Landing.tsx
@@ -26,8 +26,8 @@ export function Landing() {
       >
         <div className="relative z-10 flex w-5/6 flex-col items-center gap-y-4 rounded-lg bg-stone-900 bg-opacity-60 px-4 py-8 backdrop-blur-sm md:p-8 md:py-12 lg:w-3/4 xl:w-1/2">
           <LandingTitleSVG className="w-3/4 " />
-          <h2 className="w-3/4 text-center text-lg font-semibold text-white lg:px-8">
-            This is a subtitle. And this is additional subtitle text that will say some more stuff.
+          <h2 className="w-3/4 text-center text-lg text-white lg:px-8">
+            A tool to analyze data from the MBTA, in advocacy for better public transit.
           </h2>
           <button
             className="rounded-md border-2 border-stone-100 px-8 py-2 text-xl font-bold italic text-stone-100 hover:bg-stone-100 hover:text-stone-700"
