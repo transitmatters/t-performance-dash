89c9300f164786738300934d03ae301269ca3d96
Patrick Cleary <patrickdaviscleary@gmail.com>
2023-06-15 11:18:35 -0400

    shrink line selection (#672)
    * simplify line selection
    
    * lint
    
    * shrink side nav vertical

--- modules/navigation/BusNavMenu.tsx
+++ modules/navigation/BusNavMenu.tsx
@@ -11,8 +11,7 @@ export const BusNavMenu: React.FC<BusNavMenuProps> = ({ setSidebarOpen }) => {
   return (
     <>
       <BusSelection setSidebarOpen={setSidebarOpen} />
-      <div className="flex flex-col gap-y-4">
-        <hr className="h-1 w-full border-stone-400" />
+      <div className="flex flex-col gap-y-2 px-1">
         <SidebarTabs tabs={BUS_OVERVIEW} setSidebarOpen={setSidebarOpen} />
         <SidebarTabs tabs={TRIP_PAGES} setSidebarOpen={setSidebarOpen} />
       </div>
