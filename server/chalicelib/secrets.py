3d4f02b3d8bbebba5221a7307ee8f3d0a47cfdd5
Preston Mueller <skierjunk@gmail.com>
2023-06-24 14:51:26 -0400

    [v3] hide secrets in healthcheck output (#697)

--- server/chalicelib/secrets.py
+++ server/chalicelib/secrets.py
@@ -2,3 +2,8 @@ import os
 
 MBTA_V2_API_KEY = os.environ.get("MBTA_V2_API_KEY", "")
 MBTA_V3_API_KEY = os.environ.get("MBTA_V3_API_KEY", "")
+
+HEALTHCHECK_HIDE_SECRETS = [
+    MBTA_V2_API_KEY,
+    MBTA_V3_API_KEY
+]
