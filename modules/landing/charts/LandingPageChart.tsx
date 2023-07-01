9467678e27b4b74ecc31b40eac9bfe6b28849c36
Patrick Cleary <patcleary11@gmail.com>
2023-07-01 10:38:33 -0400

    wip

--- modules/landing/charts/LandingPageChart.tsx
+++ modules/landing/charts/LandingPageChart.tsx
@@ -8,7 +8,7 @@ import { enUS } from 'date-fns/locale';
 import { COLORS } from '../../../common/constants/colors';
 import { THREE_MONTHS_AGO_STRING, TODAY_STRING } from '../../../common/constants/dates';
 import { SPEED_RANGE_PARAM_MAP } from '../../speed/constants/speeds';
-import { HEAVY_RAIL_LINES } from '../../../common/types/lines';
+import { HEAVY_RAIL_LINES, LANDING_RAIL_LINES } from '../../../common/types/lines';
 import { LINE_OBJECTS } from '../../../common/constants/lines';
 
 interface LandingPageChartsProps {
@@ -42,7 +42,7 @@ export const LandingPageChart: React.FC<LandingPageChartsProps> = ({ datasets, l
                 callbacks: {
                   label: (value) =>
                     `${value.formattedValue}% of baseline (${
-                      LINE_OBJECTS[HEAVY_RAIL_LINES[value.datasetIndex]].name
+                      LINE_OBJECTS[LANDING_RAIL_LINES[value.datasetIndex]].name
                     })`,
                   ...callbacks,
                 },
