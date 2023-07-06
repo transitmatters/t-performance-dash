95d9ff8add98d9bf853f60c122c3b6d56ae31f9b
Patrick Cleary <patcleary11@gmail.com>
2023-07-06 10:22:58 -0400

    rounding bug

--- common/utils/time.tsx
+++ common/utils/time.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import dayjs from 'dayjs';
-import type { DurationUnitType } from 'dayjs/plugin/duration';
 import { WidgetText } from '../components/widgets/internal/WidgetText';
 import { UnitText } from '../components/widgets/internal/UnitText';
 
@@ -46,28 +45,16 @@ export const getFormattedTimeValue = (value: number) => {
   }
 };
 
-export const getFormattedTimeString = (value: number, unit: DurationUnitType = 'seconds') => {
-  if (unit === 'seconds') {
-    const absValue = Math.round(Math.abs(value));
-    const duration = dayjs.duration(absValue, unit);
-    switch (true) {
-      case absValue < 100:
-        return `${absValue}s`;
-      case absValue < 3600:
-        return `${duration.format('m')}m ${duration.format('s').padStart(2, '0')}s`;
-      default:
-        return `${duration.format('H')}h ${duration.format('m').padStart(2, '0')}m`;
-    }
-  } else if (unit === 'minutes') {
-    const absValue = Math.abs(value);
-    const duration = dayjs.duration(absValue, unit);
-    switch (true) {
-      case absValue < 1:
-        return `${duration.format('s')}s`;
-      case absValue < 60:
-        return `${duration.format('m')}m ${duration.format('s').padStart(2, '0')}s`;
-      default:
-        return `${duration.format('H')}h ${duration.format('m').padStart(2, '0')}m`;
-    }
+export const getFormattedTimeString = (value: number, unit: 'minutes' | 'seconds') => {
+  const secondsValue = unit === 'seconds' ? value : value * 60;
+  const absValue = Math.round(Math.abs(secondsValue));
+  const duration = dayjs.duration(absValue, 'seconds');
+  switch (true) {
+    case absValue < 100:
+      return `${absValue}s`;
+    case absValue < 3600:
+      return `${duration.format('m')}m ${duration.format('s').padStart(2, '0')}s`;
+    default:
+      return `${duration.format('H')}h ${duration.format('m').padStart(2, '0')}m`;
   }
 };
