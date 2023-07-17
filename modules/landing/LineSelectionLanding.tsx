1b27be0acbea7a47f43a38db2dc71d4f11eac327
Ian Reynolds <idreyn@gmail.com>
2023-07-17 11:48:57 -0400

    update: Landing page copy and viz tweaks

--- modules/landing/LineSelectionLanding.tsx
+++ modules/landing/LineSelectionLanding.tsx
@@ -8,8 +8,8 @@ export const LineSelectionLanding: React.FC = () => {
   return (
     <div className="flex w-full flex-col items-start px-4 pt-20 text-stone-900 md:items-center md:pt-32">
       <h2 className="text-5xl font-thin xl:text-7xl">Ready to learn more?</h2>
-      <h3 className="text-lg font-thin">Select a subway line or bus route to get started.</h3>
-      <div className="flex w-full flex-col gap-y-8 pt-12 md:flex-row md:justify-around">
+      <h3 className="text-lg">Select a subway line or bus route to get started.</h3>
+      <div className="flex w-full flex-col gap-y-8 pt-12 md:max-w-4xl md:flex-row md:justify-around">
         <LineButton line="line-red">
           <FontAwesomeIcon
             icon={faTrainSubway}
