61b003c5365dd255ae457a4dc577aeb72fb6524f
Patrick Cleary <patcleary11@gmail.com>
2023-10-21 18:09:51 -0400

    lint is broken...

--- modules/navigation/SidebarTabs.tsx
+++ modules/navigation/SidebarTabs.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import {
   useDelimitatedRoute,
