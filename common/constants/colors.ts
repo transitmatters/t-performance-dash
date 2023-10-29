ecf685720b424e94803177577372b41a0ea482cd
Devin Matte <devinmatte@gmail.com>
2023-10-29 18:21:58 -0400

    Swapped to lighter colors

--- common/constants/colors.ts
+++ common/constants/colors.ts
@@ -51,10 +51,10 @@ export const LINE_COLORS = {
 };
 
 export const LINE_COLORS_LEVELS = {
-  'line-red': { 0: '#D13434', 1: '#c02c2c', 2: '#ab2727', 3: '#962222' },
-  'line-orange': { 0: '#ed8b00', 1: '#d47c00', 2: '#ba6d00', 3: '#a15e00' },
-  'line-blue': { 0: '#003da5', 1: '#00348c', 2: '#002a72', 3: '#002159' },
-  'line-green': { 0: '#00834d', 1: '#006a3e', 2: '#00502f', 3: '#003720' },
-  'line-bus': { 0: '#ffc72c', 1: '#ffc013', 2: '#f8b600', 3: '#dfa300' },
+  'line-red': { 0: '#D13434', 1: '#d64949', 2: '#da5e5e', 3: '#df7272' },
+  'line-orange': { 0: '#ed8b00', 1: '#ff9907', 2: '#ffa321', 3: '#ffae3b' },
+  'line-blue': { 0: '#003da5', 1: '#0046bf', 2: '#0050d8', 3: '#0059f2' },
+  'line-green': { 0: '#00834d', 1: '#009d5c', 2: '#00b66b', 3: '#00d07a' },
+  'line-bus': { 0: '#ffc72c', 1: '#ffce46', 2: '#ffd55f', 3: '#ffdb79' },
   default: '#303030',
 };
