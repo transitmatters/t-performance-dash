deb071da8a0753f6c64ef46ccdce04c7f49ed231
Patrick Cleary <patcleary11@gmail.com>
2023-10-29 09:15:04 -0400

    Fix bug with duplicate dropdowns

--- common/components/nav/SubwayDropdown.tsx
+++ common/components/nav/SubwayDropdown.tsx
@@ -2,8 +2,9 @@ import { faSubway, faTrainSubway, faTrainTram, faTram } from '@fortawesome/free-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Transition } from '@headlessui/react';
 import classNames from 'classnames';
+import { delay } from 'lodash';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SidebarTabs } from '../../../modules/navigation/SidebarTabs';
 import { LINE_COLORS } from '../../constants/colors';
 import { LINE_OBJECTS } from '../../constants/lines';
@@ -17,10 +18,24 @@ interface SubwayDropdownProps {
     line: Line;
     route: Route;
 }
+const Dropdown: React.FC = ({ }) => {
+    return <div className={"flex rounded-b-md bg-neutral-800 flex-col px-1 gap-[2px] py-[2px]"} role={'navigation'}>
+        <SidebarTabs tabs={OVERVIEW_PAGE} />
+        <hr className="border-neutral-500 h-[1px] w-3/4 self-center" />
+        <SidebarTabs tabs={LINE_PAGES} />
+        <hr className="border-neutral-500 h-[1px] w-3/4 self-center" />
+        <SidebarTabs tabs={TRIP_PAGES} />
+    </div>
+}
 
 export const SubwayDropdown: React.FC<SubwayDropdownProps> = ({ line, route }) => {
-
     const selected = line === route.line
+    const [show, setShow] = useState<boolean>(false)
+
+    // Need a timeout so that the selected and previously selected dropdowns do not render as opened simultaneously.
+    useEffect(() => {
+        setTimeout(() => setShow(true), 0)
+    }, [selected])
     return <div className={classNames('w-full')}>
         <Link href={getLineSelectionItemHref(line, route)} >
             <div className={classNames('w-full px-2 gap-2 flex py-1 items-center flex-row rounded-t-md', selected ? lineColorBackground[line ?? 'DEFAULT'] : '')}>
@@ -28,19 +43,7 @@ export const SubwayDropdown: React.FC<SubwayDropdownProps> = ({ line, route }) =
                 {LINE_OBJECTS[line].name}
             </div>
         </Link>
-        <Transition show={selected}
-            enter="transition-opacity duration-150"
-            enterFrom="opacity-0"
-            enterTo="opacity-100">
-            <div className={"flex rounded-b-md bg-neutral-800 flex-col px-1 gap-[2px] py-[2px]"} role={'navigation'}>
-
-                <SidebarTabs tabs={OVERVIEW_PAGE} />
-                <hr className="border-neutral-500 h-[1px] w-3/4 self-center" />
-                <SidebarTabs tabs={LINE_PAGES} />
-                <hr className="border-neutral-500 h-[1px] w-3/4 self-center" />
-                <SidebarTabs tabs={TRIP_PAGES} />
-            </div>
-        </Transition>
+        {selected && show && <Dropdown />}
     </div >
 
 }
