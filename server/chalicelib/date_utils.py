9532a70a97d9dcb18b3e57e7b6ce424a970fa4c5
Devin Matte <devinmatte@gmail.com>
2023-12-22 13:09:28 -0500

    Inject timezone into non-timezone datestamps

--- server/chalicelib/date_utils.py
+++ server/chalicelib/date_utils.py
@@ -1,14 +1,16 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 
 DATE_FORMAT_MASSDOT = "%Y-%m-%d %H:%M:%S"
 DATE_FORMAT_TIMEZONE = "%Y-%m-%d %H:%M:%S%z"
 DATE_FORMAT_OUT = "%Y-%m-%dT%H:%M:%S"
+EASTERN_TIME = ZoneInfo("US/Eastern")
 
 
 def parse_event_date(date_str: str):
     if len(date_str) == 19:
-        return datetime.strptime(date_str, DATE_FORMAT_MASSDOT)
+        return datetime.strptime(date_str, DATE_FORMAT_MASSDOT).replace(tzinfo=EASTERN_TIME)
     else:
         return datetime.strptime(date_str, DATE_FORMAT_TIMEZONE)
 
