8e4213f9c283bd04c586b0b837974fa34c8fba39
Devin Matte <devinmatte@gmail.com>
2023-12-27 18:29:45 -0500

    Cleanup code ahead of gobble

--- common/components/nav/NavSectionHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface NavSectionHeaderProps {
-  title: string;
-}
-
-export const NavSectionHeader: React.FC<NavSectionHeaderProps> = ({ title }) => {
-  return <h3 className="text-xs italic text-stone-300">{title}</h3>;
-};
