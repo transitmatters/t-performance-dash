216d05ac19177b415f0ee180dac4395cad4bf408
Ian Reynolds <idreyn@gmail.com>
2024-06-18 06:55:18 -0400

    lint

--- common/utils/array.ts
+++ common/utils/array.ts
@@ -1,10 +1,10 @@
 export const indexByProperty = <T extends { [key: string]: any }>(
-    array: T[],
-    property: keyof T
+  array: T[],
+  property: keyof T
 ) => {
-    const res: Record<string, T> = {};
-    array.forEach((el) => {
-        res[el[property]] = el;
-    });
-    return res;
+  const res: Record<string, T> = {};
+  array.forEach((el) => {
+    res[el[property]] = el;
+  });
+  return res;
 };
