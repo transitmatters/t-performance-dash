c5b9519528ebc7de78f5a2fcef33692b0d2d8a83
Andrew Kouré <andrew@koure.org>
2026-02-15 03:12:25 +0000

    add docstrings to service

--- server/chalicelib/service_ridership_dashboard.py
+++ server/chalicelib/service_ridership_dashboard.py
@@ -1,9 +1,12 @@
+"""Service ridership dashboard data retrieval from S3."""
+
 import boto3
 
 bucket = boto3.resource("s3").Bucket("tm-service-ridership-dashboard")
 
 
 def get_service_ridership_dash_json():
+    """ """
     obj = bucket.Object("latest.json")
     body = obj.get()["Body"].read()
     return body.decode("utf-8")
