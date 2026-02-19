1e141e09f963b3a9d2f31176bf1a2dc806bedd97
Andrew Kouré <andrew@koure.org>
2026-02-19 04:42:19 +0000

    Updated Service Ridership Dashboard docstring

--- server/chalicelib/service_ridership_dashboard.py
+++ server/chalicelib/service_ridership_dashboard.py
@@ -6,7 +6,11 @@ bucket = boto3.resource("s3").Bucket("tm-service-ridership-dashboard")
 
 
 def get_service_ridership_dash_json():
-    """ """
+    """Fetch the latest service ridership dashboard JSON from S3.
+
+    Returns:
+        str: The decoded JSON string from the latest service ridership dashboard file.
+    """
     obj = bucket.Object("latest.json")
     body = obj.get()["Body"].read()
     return body.decode("utf-8")
